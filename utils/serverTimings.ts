ddadb134aadf5260eebb6a8015c7df426c7e26fc
Guilherme Rodrigues <gui@futuredriven.dev>
2022-08-11 19:16:28 -0300

    Allow any label

--- utils/serverTimings.ts
+++ utils/serverTimings.ts
@@ -1,6 +1,6 @@
 type Timing = { start: number; end?: number };
 
-type TimingKey = "fetch-page-data" | "render"
+type TimingKey = string;
 
 export function createServerTiming() {
   const timings: Record<string, Timing> = {};
