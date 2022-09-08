001d433fb7f9dae786629b2800b8557432220140
igorbrasileiro <brasileiro456@gmail.com>
2022-09-08 15:15:05 -0300

    Create Modal Component

---
+++ src/ui/Modal.tsx
@@ -0,0 +1,91 @@
+import type { h } from "preact";
+import { useRef } from "preact/hooks";
+import { createPortal } from "preact/compat";
+import useTrapFocus from "./hooks/useTrapFocus.tsx";
+import { tw } from "twind";
+
+interface ModalContentProps extends h.JSX.HTMLAttributes<HTMLDivElement> {}
+
+function ModalContent({ children, ...props }: ModalContentProps) {
+  const trapFocusRef = useRef<HTMLDivElement>(null);
+  const beforeElementRef = useRef<HTMLDivElement>(null);
+  const afterElementRef = useRef<HTMLDivElement>(null);
+
+  useTrapFocus({
+    trapFocusRef,
+    beforeElementRef,
+    afterElementRef,
+  });
+
+  return (
+    <>
+      <div
+        ref={beforeElementRef}
+        tabIndex={0}
+        aria-hidden="true"
+      />
+      <div
+        ref={trapFocusRef}
+        aria-modal="true"
+        role="dialog"
+        tabIndex={-1}
+        {...props}
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        {children}
+      </div>
+      <div
+        ref={afterElementRef}
+        tabIndex={0}
+        aria-hidden="true"
+      />
+    </>
+  );
+}
+
+interface Props extends ModalContentProps {
+  open: boolean;
+  onDismiss?: (event: MouseEvent | KeyboardEvent) => void;
+  modalProps?: Omit<
+    h.JSX.HTMLAttributes<HTMLDivElement>,
+    "onClick" | "onKeyDown"
+  >;
+}
+
+export default function Modal(
+  { children, open, modalProps = {}, onDismiss, ...props }: Props,
+) {
+  const handleBackdropClick = (event: MouseEvent) => {
+    if (event.defaultPrevented) {
+      return;
+    }
+
+    event.stopPropagation();
+    onDismiss?.(event);
+  };
+
+  const handleBackdropKeyDown = (event: KeyboardEvent) => {
+    if (event.key !== "Escape" || event.defaultPrevented) {
+      return;
+    }
+
+    event.stopPropagation();
+    onDismiss?.(event);
+  };
+
+  return open
+    ? createPortal(
+      <div
+        {...modalProps}
+        class={tw`bg-gray-500 bg-opacity-50 ${modalProps?.class}`}
+        onKeyDown={handleBackdropKeyDown}
+        onClick={handleBackdropClick}
+      >
+        <ModalContent {...props}>{children}</ModalContent>
+      </div>,
+      document.body,
+    )
+    : null;
+}
