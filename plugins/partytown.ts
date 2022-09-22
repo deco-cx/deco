749d7cadc59d3e9c7e176fa162c4b5a49362e2c8
igorbrasileiro <brasileiro456@gmail.com>
2022-09-22 10:56:45 -0300

    Handle partytown config to partytown plugin

--- plugins/partytown.ts
+++ plugins/partytown.ts
@@ -1,9 +1,10 @@
 import { Plugin } from "$fresh/server.ts";
-import { partytownSnippet } from "partytown";
+import { PartytownConfig, partytownSnippet } from "partytown";
 
-export default function partytown(): Plugin {
-  const main =
-    `data:application/javascript,export default function(state){${partytownSnippet()}}`;
+export default function partytown(config: PartytownConfig): Plugin {
+  const main = `data:application/javascript,export default function(state){${
+    partytownSnippet(config)
+  }}`;
   return {
     name: "partytown",
     entrypoints: { "main": main },
