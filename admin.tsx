fc63a0f17d5e78bc4ecbc2dc7e6e5e3ea6d1bc3a
Guilherme Rodrigues <gui@futuredriven.dev>
2022-10-07 13:56:07 -0300

    Improve table

--- admin.tsx
+++ admin.tsx
@@ -4,8 +4,197 @@ import { getSupabaseClientForUser } from "$live/supabase.ts";
 import { createPrivateHandler } from "$live/auth.tsx";
 import LiveContext from "./context.ts";
 
+const LiveBadge = () => (
+  <div class="flex items-center">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="18"
+      height="18"
+      fill="#003232"
+      viewBox="0 0 256 256"
+      class="mr-1"
+    >
+      <rect width="256" height="256" fill="none"></rect>
+      <circle
+        cx="128"
+        cy="128"
+        r="96"
+        fill="none"
+        stroke="#003232"
+        stroke-miterlimit="10"
+        stroke-width="16"
+      >
+      </circle>
+      <line
+        x1="32"
+        y1="128"
+        x2="224"
+        y2="128"
+        fill="none"
+        stroke="#003232"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="16"
+      >
+      </line>
+      <ellipse
+        cx="128"
+        cy="128"
+        rx="40"
+        ry="93.4"
+        fill="none"
+        stroke="#003232"
+        stroke-miterlimit="10"
+        stroke-width="16"
+      >
+      </ellipse>
+    </svg>
+    <span>
+      Live
+    </span>
+  </div>
+);
+
+const DraftBadge = () => (
+  <div class="flex items-center">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="18"
+      height="18"
+      fill="#003232"
+      class="mr-1"
+      viewBox="0 0 256 256"
+    >
+      <rect width="256" height="256" fill="none"></rect>
+      <path
+        d="M72,224H56a8,8,0,0,1-8-8V184"
+        fill="none"
+        stroke="#003232"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="16"
+      >
+      </path>
+      <polyline
+        points="120 32 152 32 208 88 208 136"
+        fill="none"
+        stroke="#003232"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="16"
+      >
+      </polyline>
+      <path
+        d="M48,64V40a8,8,0,0,1,8-8H80"
+        fill="none"
+        stroke="#003232"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="16"
+      >
+      </path>
+      <polyline
+        points="152 32 152 88 208 88"
+        fill="none"
+        stroke="#003232"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="16"
+      >
+      </polyline>
+      <path
+        d="M208,176v40a8,8,0,0,1-8,8h-8"
+        fill="none"
+        stroke="#003232"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="16"
+      >
+      </path>
+      <line
+        x1="48"
+        y1="104"
+        x2="48"
+        y2="144"
+        fill="none"
+        stroke="#003232"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="16"
+      >
+      </line>
+      <line
+        x1="112"
+        y1="224"
+        x2="152"
+        y2="224"
+        fill="none"
+        stroke="#003232"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="16"
+      >
+      </line>
+    </svg>
+    <span>
+      Draft
+    </span>
+  </div>
+);
+
+const ExperimentBadge = () => (
+  <div class="flex items-center">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="18"
+      height="18"
+      fill="#003232"
+      class="mr-1"
+      viewBox="0 0 256 256"
+    >
+      <rect width="256" height="256" fill="none"></rect>
+      <path
+        d="M104,32V93.8a8.4,8.4,0,0,1-1.1,4.1l-63.6,106A8,8,0,0,0,46.1,216H209.9a8,8,0,0,0,6.8-12.1l-63.6-106a8.4,8.4,0,0,1-1.1-4.1V32"
+        fill="none"
+        stroke="#003232"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="16"
+      >
+      </path>
+      <line
+        x1="88"
+        y1="32"
+        x2="168"
+        y2="32"
+        fill="none"
+        stroke="#003232"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="16"
+      >
+      </line>
+      <path
+        d="M62.6,165c11.8-8.7,32.1-13.6,65.4,3,35.7,17.9,56.5,10.8,67.9,1.1"
+        fill="none"
+        stroke="#003232"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="16"
+      >
+      </path>
+    </svg>
+    <span>
+      Experiment
+    </span>
+  </div>
+);
+
 const PageRow = (props: { page: any }) => {
   const { page } = props;
+  const pageLink = `${page.path}?editor${
+    page.flag ? `&variantId=${page.id}` : ""
+  }`;
   return (
     <tr class="hover:bg-primary-light">
       <td class="border-b border-primary-light p-4 pl-8 text-primary-dark">
@@ -13,7 +202,7 @@ const PageRow = (props: { page: any }) => {
       </td>
       <td class="border-b border-primary-light text-primary-dark">
         <a
-          href={`${page.path}?editor&variantId=${page.id}`}
+          href={pageLink}
           target="_blank"
           class="w-full h-full block p-4 pl-8"
         >
@@ -24,18 +213,18 @@ const PageRow = (props: { page: any }) => {
         {page.name}
       </td>
       <td class="border-b border-primary-light p-4 pl-8 text-primary-dark">
-        {page.created_at}
-      </td>
-      <td class="border-b border-primary-light p-4 pl-8 text-primary-dark">
         {page.flag
-          ? page.flag.traffic === 0 ? "Draft" : "Experiment"
-          : "Production"}
+          ? page.flag.traffic === 0 ? <DraftBadge /> : <ExperimentBadge />
+          : <LiveBadge />}
       </td>
       <td class="border-b border-primary-light p-4 pl-8 text-primary-dark">
         {page.flag?.traffic}
       </td>
       <td class="border-b border-primary-light p-4 pl-8 text-primary-dark">
         {JSON.stringify(page.archived)}
+      </td>
+      <td class="border-b border-primary-light p-4 pl-8 text-primary-dark">
+        {page.created_at}
       </td>
     </tr>
   );
@@ -51,7 +240,7 @@ export default function LiveAdmin(props: PageProps<any>) {
         </p>
       </div>
 
-      <h2 class="mb-2 text-xl font-bold">My pages</h2>
+      <h2 class="mb-2 font-bold">Pages</h2>
       <table class="border-collapse table-fixed w-full text-sm text-primary-dark">
         <thead>
           <tr>
@@ -65,16 +254,16 @@ export default function LiveAdmin(props: PageProps<any>) {
               name
             </th>
             <th class="border-b border-primary-light font-medium p-4 pl-8 pt-0 pb-3 text-left">
-              created
-            </th>
-            <th class="border-b border-primary-light font-medium p-4 pl-8 pt-0 pb-3 text-left">
               status
             </th>
-            <th class="border-b border-primary-light font-medium p-4 pl-8 pt-0 pb-3 text-left">
+            <th class="border-b border-primary-light font-medium p-4 pl-8 pt-0 pb-3 text-left ">
               traffic
             </th>
             <th class="border-b border-primary-light font-medium p-4 pl-8 pt-0 pb-3 text-left">
               archived
+            </th>
+            <th class="border-b border-primary-light font-medium p-4 pl-8 pt-0 pb-3 text-left">
+              created
             </th>
           </tr>
         </thead>
