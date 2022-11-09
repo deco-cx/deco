2e1a1868e231804f7fc6cc5b071c4559c23e8915
Lucis <luciannojunior@gmail.com>
2022-11-08 22:17:33 -0300

    Creating VTEX clients

--- shared/commerce/types/Product.ts
+++ shared/commerce/types/Product.ts
@@ -4,17 +4,23 @@ export interface Image {
 }
 
 export interface Product {
+  productId: string;
   id: string;
   name: string;
   sellerId: string;
   price: number;
+  listPrice: number;
   installments: string;
   image: Image;
   imageHover?: Image;
+  images: Image[];
   slug: string;
   brand: string;
   description: string;
-  atributos: string;
-  nome_produto: string;
+  // atributos: string;
+  // nome_produto: string;
   breadcrumb: Array<{ label: string; url: string }>;
+  specifications: Record<string, string>;
+  skuOptions: Array<{ variationValue: string; skuUrl: string }>;
+  color?: string;
 }
