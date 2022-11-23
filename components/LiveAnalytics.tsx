6b8dc500c57b90be12dca4b0d58eee9e5e568743
Tiago Gimenes <tlgimenes@gmail.com>
2022-11-23 13:05:20 -0300

    add site_id to pageview (#56)

--- components/LiveAnalytics.tsx
+++ components/LiveAnalytics.tsx
@@ -1,23 +1,39 @@
-import Script from "https://deno.land/x/partytown@0.1.0/Script.tsx";
+import Script from "https://deno.land/x/partytown@0.1.3/Script.tsx";
 import { Page, Site } from "$live/types.ts";
 
 const innerHtml = ({ id, path }: Page, { id: siteId }: Site) => `
 import { onCLS, onFID, onLCP } from "https://esm.sh/web-vitals@3.1.0";
 
 function onWebVitalsReport(event) {
-  if (typeof window.jitsu === "function") {
-    window.jitsu('track', 'web-vitals', { ...event, page_id: "${id}", page_path: "${path}", site_id: "${siteId}" });
-  }
+  window.jitsu('track', 'web-vitals', event);
 };
 
-onCLS(onWebVitalsReport);
-onFID(onWebVitalsReport);
-onLCP(onWebVitalsReport);
+function init() {
+  if (typeof window.jitsu !== "function") {
+    return;
+  }
+
+  /* Add these trackers to all analytics sent to our server */
+  window.jitsu('set', { page_id: "${id}", page_path: "${path}", site_id: "${siteId}" });
+  /* Send page-view event */
+  window.jitsu('track', 'pageview');
+
+  /* Listen web-vitals */
+  onCLS(onWebVitalsReport);
+  onFID(onWebVitalsReport);
+  onLCP(onWebVitalsReport);
+};
+
+if (document.readyState === 'complete') {
+  init();
+} else {
+  window.addEventListener('load', init);
+};
 `;
 
 interface Props {
   page: Page;
-  site: Site
+  site: Site;
 }
 
 function LiveAnalytics({ page, site }: Props) {
