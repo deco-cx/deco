111d1cdfacb4dfc007440d8e2a910baced588481
Tiago Gimenes <tlgimenes@gmail.com>
2022-11-26 13:44:42 -0300

    explicit export

--- std/ui/components/Picture.tsx
+++ std/ui/components/Picture.tsx
@@ -23,57 +23,57 @@ type SourceProps =
     fetchPriority?: "high" | "low" | "auto";
   };
 
-const Source = forwardRef<HTMLSourceElement, SourceProps>((props, ref) => {
-  const { preload } = useContext(Context);
+export const Source = forwardRef<HTMLSourceElement, SourceProps>(
+  (props, ref) => {
+    const { preload } = useContext(Context);
 
-  const srcSet = getSrcSet(props.src, props.width, props.height);
-  const linkProps = {
-    imagesrcset: srcSet,
-    imagesizes: props.sizes,
-    fetchpriority: props.fetchPriority,
-    media: props.media,
-  };
+    const srcSet = getSrcSet(props.src, props.width, props.height);
+    const linkProps = {
+      imagesrcset: srcSet,
+      imagesizes: props.sizes,
+      fetchpriority: props.fetchPriority,
+      media: props.media,
+    };
 
-  return (
-    <>
-      {preload && (
-        <Head>
-          <link
-            as="image"
-            rel="preload"
-            href={props.src}
-            {...linkProps}
-          />
-        </Head>
-      )}
-      <source
-        {...props}
-        preload={undefined}
-        src={undefined} // Avoid deprecated api lighthouse warning
-        srcSet={srcSet}
-        ref={ref}
-      />
-    </>
-  );
-});
+    return (
+      <>
+        {preload && (
+          <Head>
+            <link
+              as="image"
+              rel="preload"
+              href={props.src}
+              {...linkProps}
+            />
+          </Head>
+        )}
+        <source
+          {...props}
+          preload={undefined}
+          src={undefined} // Avoid deprecated api lighthouse warning
+          srcSet={srcSet}
+          ref={ref}
+        />
+      </>
+    );
+  },
+);
 
 type Props = Omit<JSX.IntrinsicElements["picture"], "preload"> & {
   children: ComponentChildren;
   preload?: boolean;
 };
 
-function Picture({ children, preload, ...props }: Props) {
-  const value = useMemo(() => ({ preload }), [preload]);
+export const Picture = forwardRef<HTMLPictureElement, Props>(
+  ({ children, preload, ...props }, ref) => {
+    const value = useMemo(() => ({ preload }), [preload]);
 
-  return (
-    <Context.Provider value={value}>
-      <picture {...props}>
-        {children}
-      </picture>
-    </Context.Provider>
-  );
-}
-
-Picture.Source = Source;
-
-export default Picture;
+    return (
+      <Context.Provider value={value}>
+        <picture {...props} ref={ref}>
+          {children}
+        </picture>
+      </Context.Provider>
+    );
+  },
+);
