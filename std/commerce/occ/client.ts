d1db62017207eb0b5b2331d23b761041e7519881
Lucis <luciannojunior@gmail.com>
2023-01-03 17:16:53 -0300

    v0 of OCC client

---
+++ std/commerce/occ/client.ts
@@ -0,0 +1,35 @@
+import { fetchAPI } from "$live/utils/fetchAPI.ts";
+import { OccProdutoPage, OccSearch } from "./types.ts";
+
+export interface Options {
+  platform: "occ";
+  baseUrl: string;
+  // TOOD: Don't know yet what it means
+  nrpp?: string;
+}
+
+export const createClient = ({
+  platform,
+  baseUrl,
+  nrpp = "12",
+}: Options) => {
+  const searchByTerm = (term: string) => {
+    return fetchAPI<OccSearch>(
+      `${baseUrl}/ccstoreui/v1/search?Ntt=${term}&Nrpp=${nrpp}`,
+    );
+  };
+
+  const productBySlug = (slug: string) => {
+    return fetchAPI<OccProdutoPage>(
+      `${baseUrl}/ccstoreui/v1/pages/produto/${slug}?dataOnly=false&cacheableDataOnly=true&productTypesRequired=false`,
+    );
+  };
+
+  return {
+    platform,
+    search: {
+      searchByTerm,
+      productBySlug,
+    },
+  };
+};
