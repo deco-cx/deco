594a481ca723efe220d04053cbd95a2e1f399a1c
Igor Brasileiro <brasileiro456@gmail.com>
2023-01-11 11:51:31 -0300

    chore: Bump supabase (#79)
    * Bump supabase@2
    
    * Bump supabase
    
    Signed-off-by: igorbrasileiro <brasileiro456@gmail.com>
    
    * Drop supabase for user
    
    Signed-off-by: igorbrasileiro <brasileiro456@gmail.com>
    
    * Remove from generic type of supabase client
    
    Signed-off-by: igorbrasileiro <brasileiro456@gmail.com>

--- supabase.ts
+++ supabase.ts
@@ -1,14 +1,12 @@
-import { getCookies } from "std/http/mod.ts";
-import { createClient, SupabaseClient, User } from "supabase";
-
-export type { User };
+import { createClient, SupabaseClient } from "supabase";
 
 let client: SupabaseClient | null = null;
 
 // From supabase docs:
 // "This key is safe to use in a browser if you have enabled Row Level Security for your tables and configured policies."
-const SUPABASE_LIVE_ENDPOINT = "https://ozksgdmyrqcxcwhnbepg.supabase.co";
-const SUPABASE_LIVE_ANON_KEY =
+export const SUPABASE_LIVE_ENDPOINT =
+  "https://ozksgdmyrqcxcwhnbepg.supabase.co";
+export const SUPABASE_LIVE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96a3NnZG15cnFjeGN3aG5iZXBnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTY3MjM3NDYsImV4cCI6MTk3MjI5OTc0Nn0.HMdsG6NZlq6dvYFCK1_tuJh38TmsqLeN8H4OktTHt_M";
 
 let userEndpoint = SUPABASE_LIVE_ENDPOINT;
@@ -24,13 +22,5 @@ export default function getSupabaseClient(accessToken?: string) {
     client = createClient(userEndpoint, accessToken || userKey);
   }
 
-  return client;
-}
-
-export function getSupabaseClientForUser(req: Request) {
-  const client = createClient(userEndpoint, userKey);
-  const cookies = getCookies(req.headers);
-  const jwt = cookies["live-access-token"];
-  client.auth.setAuth(jwt);
   return client;
 }
