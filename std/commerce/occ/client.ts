9d5c2adf2f082abc5232df96f478c046d1cd7491
Paulo Ferraz <paulo.ferraz+github@acct.global>
2023-01-11 19:40:16 -0300

    feat: searchCategory and stockStatus

--- std/commerce/occ/client.ts
+++ std/commerce/occ/client.ts
@@ -1,5 +1,10 @@
 import { fetchAPI } from "$live/utils/fetchAPI.ts";
-import { OccProductPage, OccSearch } from "./types.ts";
+import {
+  CategorySearchResult,
+  OccProductPage,
+  OccSearch,
+  ProductSkuInventoryStatus,
+} from "./types.ts";
 
 export interface Options {
   platform: "occ";
@@ -8,20 +13,47 @@ export interface Options {
   nrpp?: string;
 }
 
-export const createClient = ({
-  platform,
-  baseUrl,
-  nrpp = "12",
-}: Options) => {
+export interface SearchCategoryArgs {
+  categoryId: string;
+  catalogId?: string;
+  limit?: number;
+  offset?: number;
+}
+
+export const createClient = ({ platform, baseUrl, nrpp = "12" }: Options) => {
   const searchByTerm = (term: string) => {
     return fetchAPI<OccSearch>(
-      `${baseUrl}/ccstoreui/v1/search?Ntt=${term}&Nrpp=${nrpp}`,
+      `${baseUrl}/ccstoreui/v1/search?Ntt=${term}&Nrpp=${nrpp}`
     );
   };
 
   const productBySlug = (slug: string) => {
     return fetchAPI<OccProductPage>(
-      `${baseUrl}/ccstoreui/v1/pages/produto/${slug}?dataOnly=false&cacheableDataOnly=true&productTypesRequired=false`,
+      `${baseUrl}/ccstoreui/v1/pages/produto/${slug}?dataOnly=false&cacheableDataOnly=true&productTypesRequired=false`
+    );
+  };
+
+  const searchByCategory = ({
+    catalogId = "cloudCatalog",
+    categoryId,
+    limit = 16,
+    offset = 0,
+  }: SearchCategoryArgs) => {
+    const params = new URLSearchParams({
+      limit: limit.toString(),
+      offset: offset.toString(),
+      catalogId,
+      categoryId,
+    });
+
+    return fetchAPI<CategorySearchResult>(
+      `${baseUrl}/ccstoreui/v1/products?${params.toString()}`
+    );
+  };
+
+  const stockStatus = (productId: string) => {
+    return fetchAPI<ProductSkuInventoryStatus>(
+      `${baseUrl}/ccstoreui/v1/stockStatus/${productId}`
     );
   };
 
@@ -30,6 +62,8 @@ export const createClient = ({
     search: {
       searchByTerm,
       productBySlug,
+      searchByCategory,
+      stockStatus,
     },
   };
 };
--- std/commerce/occ/transform.ts
+++ std/commerce/occ/transform.ts
@@ -1,44 +1,78 @@
 import type {
   BreadcrumbList,
   ImageObject,
+  Offer,
   Product,
   ProductDetailsPage,
 } from "../types.ts";
 
-import { Product as ProductOracle } from "./types.ts";
+import {
+  Product as ProductOracle,
+  ProductSkuInventoryStatus,
+} from "./types.ts";
+
+const setAvailabilityOffer = (
+  stock: ProductSkuInventoryStatus,
+  productId: string
+) => {
+  const offer: Offer[] = [];
+
+  for (const property in stock) {
+    offer.push({
+      "@type": "Offer",
+      price: 0,
+      priceSpecification: [],
+      additionalType: property.replace(productId, ""),
+      availability:
+        stock[property] > 0
+          ? "https://schema.org/InStock"
+          : "https://schema.org/OutOfStock",
+      inventoryLevel: {
+        value: stock[property],
+      },
+    });
+  }
+
+  return offer;
+};
 
 export const toProductPage = (
   product: ProductOracle,
+  stock: ProductSkuInventoryStatus
 ): ProductDetailsPage => {
   return {
     breadcrumbList: toBreadcrumbList(product),
-    product: toProduct(product),
+    product: toProduct(product, stock),
   };
 };
 
-export const toBreadcrumbList = (
-  product: ProductOracle,
-): BreadcrumbList => {
+export const toBreadcrumbList = (product: ProductOracle): BreadcrumbList => {
   return {
     "@type": "BreadcrumbList",
     numberOfItems: 1,
-    itemListElement: [{
-      "@type": "ListItem",
-      name: product.displayName,
-      item: product.seoUrlSlugDerived,
-      position: 1,
-    }],
+    itemListElement: [
+      {
+        "@type": "ListItem",
+        name: product.displayName,
+        item: product.seoUrlSlugDerived,
+        position: 1,
+      },
+    ],
   };
 };
 
 export const toProduct = (
   product: ProductOracle,
+  stock: ProductSkuInventoryStatus
 ): Product => {
   const {
     displayName,
-    description,
     largeImageURLs: images,
     id: productID,
+    listPrice,
+    longDescription,
+    _comp,
+    x_prazosDeEntregaEDevolues,
   } = product;
 
   return {
@@ -46,12 +80,42 @@ export const toProduct = (
     productID,
     url: product.seoUrlSlugDerived,
     name: displayName,
-    description,
     sku: productID,
-
-    image: images.map((img): ImageObject => ({
-      "@type": "ImageObject",
-      url: `https://osklen.com.br/${img}`,
-    })),
+    offers: {
+      "@type": "AggregateOffer",
+      highPrice: listPrice,
+      lowPrice: listPrice,
+      offerCount: 1,
+      offers: stock ? setAvailabilityOffer(stock, productID) : [],
+    },
+    additionalProperty: [
+      {
+        "@type": "PropertyValue",
+        name: "descrição",
+        value: longDescription,
+      },
+      {
+        "@type": "PropertyValue",
+        name: "composição",
+        value: _comp,
+      },
+      {
+        "@type": "PropertyValue",
+        name: "entregas e devoluções",
+        value: x_prazosDeEntregaEDevolues,
+      },
+      {
+        "@type": "PropertyValue",
+        name: "sobre a marca",
+        value:
+          "OSKLEN é a expressão de um lifestyle genuíno definido pelo equilíbrio de uma vida urbana integrada à natureza em que o orgânico e o tecnológico, o local e o global, o luxo e a simplicidade são complementares.",
+      },
+    ],
+    image: images.map(
+      (img): ImageObject => ({
+        "@type": "ImageObject",
+        url: `https://osklen.com.br/${img}`,
+      })
+    ),
   };
 };
