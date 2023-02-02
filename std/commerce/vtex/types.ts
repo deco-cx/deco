6642bfa2e51c958844dbe7ececea4f62acd40c06
Maria Luiza Aires <marialuizafaires@gmail.com>
2023-02-01 22:26:15 -0300

    WIP legacy search loader

--- std/commerce/vtex/types.ts
+++ std/commerce/vtex/types.ts
@@ -79,6 +79,233 @@ export interface ProductSearchResult {
   fuzzy: string;
   correction?: Correction;
 }
+// ---------- VTEX LEGACY SEARCH RESPONSE
+// export interface VTEXLegacyProduct{
+//   productId: string;
+//   productName: string;
+//   brand: string;
+//   brandId: number;
+//   brandImageUrl: null;
+//   linkText: string;
+//   productReference: string;
+//   productReferenceCode: string;
+//   categoryId: string;
+//   productTitle: string;
+//   metaTagDescription: string;
+//   releaseDate: Date;
+//   clusterHighlights: ClusterHighlights;
+//   productClusters: { [key: string]: string };
+//   searchableClusters: { [key: string]: string };
+//   categories: string[];
+//   categoriesIds: string[];
+//   link: string;
+//   Marca: string[];
+//   "Tipo de Produto": string[];
+//   SALE: string[];
+//   Coleção: string[];
+//   "ID Coleção": string[];
+//   "Coleção Atual": string[];
+//   "Tabela de Medidas": string[];
+//   Tamanho: string[];
+//   Composição: string[];
+//   Cor: string[];
+//   "Cores Filtráveis": string[];
+//   allSpecifications: string[];
+//   allSpecificationsGroups: string[];
+//   description: string;
+//   items: Item[];
+//   skuSpecifications: SkuSpecification[];
+// }
+
+// export interface ClusterHighlights {
+//   "1249": string;
+// }
+
+// export interface Item {
+//   itemId: string;
+//   name: string;
+//   nameComplete: string;
+//   complementName: string;
+//   ean: string;
+//   referenceId: ReferenceID[];
+//   measurementUnit: string;
+//   unitMultiplier: number;
+//   modalType: null;
+//   isKit: boolean;
+//   images: Image[];
+//   Tamanho: string[];
+//   variations: string[];
+//   sellers: Seller[];
+//   Videos: any[];
+//   estimatedDateArrival: null;
+// }
+
+// export interface Image {
+//   imageId: string;
+//   imageLabel: string;
+//   imageTag: string;
+//   imageUrl: string;
+//   imageText: ImageText;
+//   imageLastModified: Date;
+// }
+
+// export enum ImageText {
+//   The52104418_5361_10TopDeLinhoRosaFucsia =
+//     "52104418_5361_10-TOP-DE-LINHO-ROSA-FUCSIA",
+//   The52104418_5361_1TopDeLinhoRosaFucsia =
+//     "52104418_5361_1-TOP-DE-LINHO-ROSA-FUCSIA",
+//   The52104418_5361_2TopDeLinhoRosaFucsia =
+//     "52104418_5361_2-TOP-DE-LINHO-ROSA-FUCSIA",
+//   The52104418_5361_3TopDeLinhoRosaFucsia =
+//     "52104418_5361_3-TOP-DE-LINHO-ROSA-FUCSIA",
+//   The52104418_5361_4TopDeLinhoRosaFucsia =
+//     "52104418_5361_4-TOP-DE-LINHO-ROSA-FUCSIA",
+// }
+
+// export interface ReferenceID {
+//   Key: string;
+//   Value: string;
+// }
+
+// export interface Seller {
+//   sellerId: string;
+//   sellerName: string;
+//   addToCartLink: string;
+//   sellerDefault: boolean;
+//   commertialOffer: CommertialOffer;
+// }
+
+// export interface CommertialOffer {
+//   DeliverySlaSamplesPerRegion: DeliverySlaSamplesPerRegion;
+//   Installments: Installment[];
+//   DiscountHighLight: any[];
+//   GiftSkuIds: any[];
+//   Teasers: any[];
+//   BuyTogether: any[];
+//   ItemMetadataAttachment: any[];
+//   Price: number;
+//   ListPrice: number;
+//   PriceWithoutDiscount: number;
+//   RewardValue: number;
+//   PriceValidUntil: Date;
+//   AvailableQuantity: number;
+//   IsAvailable: boolean;
+//   Tax: number;
+//   DeliverySlaSamples: DeliverySlaSample[];
+//   GetInfoErrorMessage: null | string;
+//   CacheVersionUsedToCallCheckout: string;
+//   PaymentOptions: PaymentOptions;
+// }
+
+// export interface DeliverySlaSample {
+//   DeliverySlaPerTypes: any[];
+//   Region: null;
+// }
+
+// export interface DeliverySlaSamplesPerRegion {
+//   "0"?: DeliverySlaSample;
+// }
+
+// export interface Installment {
+//   Value: number;
+//   InterestRate: number;
+//   TotalValuePlusInterestRate: number;
+//   NumberOfInstallments: number;
+//   PaymentSystemName: Name;
+//   PaymentSystemGroupName: GroupName;
+//   Name: string;
+// }
+
+// export enum GroupName {
+//   CreditCardPaymentGroup = "creditCardPaymentGroup",
+//   GiftCardPaymentGroup = "giftCardPaymentGroup",
+//   InstantPaymentPaymentGroup = "instantPaymentPaymentGroup",
+// }
+
+// export enum Name {
+//   AmericanExpress = "American Express",
+//   Diners = "Diners",
+//   Elo = "Elo",
+//   Hipercard = "Hipercard",
+//   Mastercard = "Mastercard",
+//   Pix = "Pix",
+//   Vale = "Vale",
+//   Visa = "Visa",
+// }
+
+// export interface PaymentOptions {
+//   installmentOptions: InstallmentOption[];
+//   paymentSystems: PaymentSystem[];
+//   payments: any[];
+//   giftCards: any[];
+//   giftCardMessages: any[];
+//   availableAccounts: any[];
+//   availableTokens: any[];
+// }
+
+// export interface InstallmentOption {
+//   paymentSystem: string;
+//   bin: null;
+//   paymentName: Name;
+//   paymentGroupName: GroupName;
+//   value: number;
+//   installments: InstallmentElement[];
+// }
+
+// export interface InstallmentElement {
+//   count: number;
+//   hasInterestRate: boolean;
+//   interestRate: number;
+//   value: number;
+//   total: number;
+//   sellerMerchantInstallments?: InstallmentElement[];
+//   id?: ID;
+// }
+
+// export enum ID {
+//   Lojaoffpremium = "LOJAOFFPREMIUM",
+// }
+
+// export interface PaymentSystem {
+//   id: number;
+//   name: Name;
+//   groupName: GroupName;
+//   validator: null;
+//   stringId: string;
+//   template: Template;
+//   requiresDocument: boolean;
+//   isCustom: boolean;
+//   description: null;
+//   requiresAuthentication: boolean;
+//   dueDate: Date;
+//   availablePayments: null;
+// }
+
+// export enum Template {
+//   CreditCardPaymentGroupTemplate = "creditCardPaymentGroup-template",
+//   GiftCardPaymentGroupTemplate = "giftCardPaymentGroup-template",
+//   InstantPaymentPaymentGroupTemplate = "instantPaymentPaymentGroup-template",
+// }
+
+// export interface SkuSpecification {
+//   field: Field;
+//   values: Value[];
+// }
+
+// export interface Field {
+//   id: number;
+//   name: string;
+//   isActive: boolean;
+//   position: number;
+//   type: string;
+// }
+
+// export interface Value {
+//   id: string;
+//   name: string;
+//   position: number;
+// }
+// FINAL ------- VTEX LEGACY SEARCH RESPONSE
 
 interface Correction {
   misspelled: boolean;
