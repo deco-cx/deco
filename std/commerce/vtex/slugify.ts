1008f4b5e05ee549f9cdcd2cc3cc6728402c2a9c
Lucis <luciannojunior@gmail.com>
2023-02-14 19:57:43 -0300

    Improving slugify algorithm

--- std/commerce/vtex/slugify.ts
+++ std/commerce/vtex/slugify.ts
@@ -1,45 +1,14 @@
-/* eslint-disable no-useless-escape */
-/**
- * VTEX catalog slugify function
- *
- * Sometimes, we need to slugify strings for creating urls. An example is the
- * brand urls, where we create them from the brand's name.
- * This slugify function should match exactly what VTEX catalog generates. Any mismatch
- * will lead to broken links.
- * Hopefully, we had this function implemented on VTEX IO and we've been using it for
- * years now. However, looking at the code, I think we can save lots of computing. I'm
- * in a hurry for doing these tests now, so I'll leave a small TODO.
- *
- * TODO: Research for better ways of computing this slugify function. Things I'd try are:
- * - Join those 3 regexs for special characters into a single one.
- * - Replace the regexp of `removeDiacritics` function with a Map. We can make the complexity
- * of this function be O(n) with n=string.length
- *
- */
- const from =
- 'ÁÄÂÀÃÅČÇĆĎÉĚËÈÊẼĔȆÍÌÎÏŇÑÓÖÒÔÕØŘŔŠŤÚŮÜÙÛÝŸŽáäâàãåčçćďéěëèêẽĕȇíìîïňñóöòôõøðřŕšťúůüùûýÿžþÞĐđßÆa'
-
-const to =
- 'AAAAAACCCDEEEEEEEEIIIINNOOOOOORRSTUUUUUYYZaaaaaacccdeeeeeeeeiiiinnooooooorrstuuuuuyyzbBDdBAa'
-
-const removeDiacritics = (str: string) => {
- let newStr = str.slice(0)
-
- for (let i = 0; i < from.length; i++) {
-   newStr = newStr.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i))
- }
-
- return newStr
-}
-
 const slugifySpecialCharacters = (str: string) => {
- return str.replace(/[·/_,:]/, '-')
-}
+  return str
+    .replace(/[·/_,:]/g, '-')
+    .replace(/[*+~.()'"!:@&\[\]`/ %$#?{}|><=_^]/g, '-')
+    .replace(
+      /Á|Ä|Â|À|Ã|Å|Č|Ç|Ć|Ď|É|Ě|Ë|È|Ê|Ẽ|Ĕ|Ȇ|Í|Ì|Î|Ï|Ň|Ñ|Ó|Ö|Ò|Ô|Õ|Ø|Ř|Ŕ|Š|Ť|Ú|Ů|Ü|Ù|Û|Ý|Ÿ|Ž|á|ä|â|à|ã|å|č|ç|ć|ď|é|ě|ë|è|ê|ẽ|ĕ|ȇ|í|ì|î|ï|ň|ñ|ó|ö|ò|ô|õ|ø|ð|ř|ŕ|š|ť|ú|ů|ü|ù|û|ý|ÿ|ž|þ|Þ|Đ|đ|ß|Æ|a/g,
+      "AAAAAACCCDEEEEEEEEIIIINNOOOOOORRSTUUUUUYYZaaaaaacccdeeeeeeeeiiiinnooooooorrstuuuuuyyzbBDdBAa"
+    )
+    .toLowerCase();
+};
 
 export function slugify(str: string) {
- const noCommas = str.replace(/,/g, '')
- const replaced = noCommas.replace(/[*+~.()'"!:@&\[\]`/ %$#?{}|><=_^]/g, '-')
- const slugified = slugifySpecialCharacters(removeDiacritics(replaced))
-
- return slugified.toLowerCase()
+  return slugifySpecialCharacters(str.replace(/,/g, ""));
 }
