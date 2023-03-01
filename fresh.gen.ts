19da6b53599c4067f3afe25903dc9340c553c12c
gimenes <tlgimenes@gmail.com>
2023-03-01 13:24:31 -0300

    hotfix: regenerate fresh.gen.ts

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -6,7 +6,6 @@ import config from "./deno.json" assert { type: "json" };
 import { context } from "$live/live.ts";
 import { DecoManifest } from "$live/types.ts";
 
-import * as $$$0 from "./sections/Head.tsx";
 import * as $$$$0 from "./functions/EffectSelectPage.ts";
 import * as $$$$1 from "./functions/MatchDate.ts";
 import * as $$$$2 from "./functions/MatchEnvironment.ts";
@@ -17,7 +16,7 @@ import * as $$$$5 from "./functions/MatchUserAgent.ts";
 const manifest: DecoManifest = {
   routes: {},
   islands: {},
-  sections: { "./sections/Head.tsx": $$$0 },
+  sections: {},
   functions: {
     "./functions/EffectSelectPage.ts": $$$$0,
     "./functions/MatchDate.ts": $$$$1,
@@ -27,86 +26,6 @@ const manifest: DecoManifest = {
     "./functions/MatchUserAgent.ts": $$$$5,
   },
   schemas: {
-    "./sections/Head.tsx": {
-      "inputSchema": {
-        "title": " Head",
-        "type": "object",
-        "properties": {
-          "title": {
-            "type": [
-              "string",
-              "null",
-            ],
-            "title": "Title",
-          },
-          "description": {
-            "type": [
-              "string",
-              "null",
-            ],
-            "title": "Description",
-          },
-          "url": {
-            "type": [
-              "string",
-              "null",
-            ],
-            "title": "Url",
-          },
-          "imageUrl": {
-            "type": [
-              "string",
-              "null",
-            ],
-            "title": "Image Url",
-          },
-          "faviconUrl": {
-            "type": [
-              "string",
-              "null",
-            ],
-            "title": "Favicon Url",
-          },
-          "styleUrls": {
-            "type": "array",
-            "items": {
-              "type": "string",
-            },
-            "title": "Style Urls",
-          },
-          "inlineStyles": {
-            "type": "array",
-            "items": {
-              "type": "string",
-            },
-            "title": "Inline Styles",
-          },
-          "scriptUrls": {
-            "type": "array",
-            "items": {
-              "type": "string",
-            },
-            "title": "Script Urls",
-          },
-          "inlineScripts": {
-            "type": "array",
-            "items": {
-              "type": "string",
-            },
-            "title": "Inline Scripts",
-          },
-          "themeColor": {
-            "type": [
-              "string",
-              "null",
-            ],
-            "title": "Theme Color",
-          },
-        },
-        "required": [],
-      },
-      "outputSchema": null,
-    },
     "./functions/EffectSelectPage.ts": {
       "inputSchema": {
         "title": " Effect Select Page",
