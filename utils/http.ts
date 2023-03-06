7e970e5458802a7e265e0aa0332a729657275cd5
gimenes <tlgimenes@gmail.com>
2023-03-06 15:04:31 -0300

    another try

--- utils/http.ts
+++ utils/http.ts
@@ -1,6 +1,7 @@
 export const DEFAULT_CACHE_CONTROL: CacheControl = {
   public: true,
-  "max-age": 60, // 10s browser cache to avoid BYPASS on cloudflare: https://developers.cloudflare.com/cache/about/default-cache-behavior/#cloudflare-cache-responses
+  "max-age": 604800, // 10s browser cache to avoid BYPASS on cloudflare: https://developers.cloudflare.com/cache/about/default-cache-behavior/#cloudflare-cache-responses
+  "immutable": true,
 };
 
 export type CacheControl = Partial<{
