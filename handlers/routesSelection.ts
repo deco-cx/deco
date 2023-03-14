3f3bfe7c1ee48d3a4dff2e183d954b30d21bff52
Marcos Candeia <marrcooos@gmail.com>
2023-03-14 19:10:24 -0300

    Add comments
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- handlers/routesSelection.ts
+++ handlers/routesSelection.ts
@@ -44,34 +44,48 @@ export type MatchWithCookieValue = MatchContext<{
 export default function RoutesSelection({ flags }: SelectionConfig): Handler {
   const audiences = flags.filter(isAudience) as AudienceFlag[];
   return async (req: Request, connInfo: ConnInfo): Promise<Response> => {
+    // Read flags from cookie or start an empty map.
     const flags =
       cookies.getFlags(req.headers) ?? new Map<string, CookiedFlag>();
 
+    // create the base match context.
     const matchCtx: Omit<MatchWithCookieValue, "isMatchFromCookie"> = {
       siteId: context.siteId,
       request: req,
     };
 
-    // flags that isn't in the original cookie or has changed
-    const newFlags: CookiedFlag[] = [];
+    // track flags that aren't on the original cookie or changed its `isMatch` property.
+    const flagsThatShouldBeCookied: CookiedFlag[] = [];
+
+    // reduce audiences building the routes and the overrides.
     const [routes, overrides] = audiences.reduce(
       ([routes, overrides], audience) => {
+        // check if the audience matches with the given context considering the `isMatch` provided by the cookies.
         const isMatch = audience.matcher({
           ...matchCtx,
           isMatchFromCookie: flags.get(audience.name)?.isMatch,
         } as MatchWithCookieValue);
+
+        // if the flag doesn't exists (e.g. new audience being used) or the `isMatch` value has changed so add as a `newFlags`
+        // TODO should we track when the flag VALUE changed?
+        // this code has a bug that when the isMatch doesn't change but the flag value does so the cookie will kept the old value.
+        // I will not fix this for now because this shouldn't be a issue in the long run and requires deep equals between objects which could be more expansive than just assume that the value is equal.
+        // as it is in 99% of the cases.
         if (
           !flags.has(audience.name) ||
           flags.get(audience.name)?.isMatch !== isMatch
         ) {
-          const currValue = {
+          // create the flag value
+          const flagValue = {
             isMatch,
             key: audience.name,
             value: audience.true,
             updated_at: new Date().toISOString(),
           };
-          newFlags.push(currValue);
-          flags.set(audience.name, currValue);
+          // set as flag that should be cookied
+          flagsThatShouldBeCookied.push(flagValue);
+          // set in the current map just in case (duplicated audiences?)
+          flags.set(audience.name, flagValue);
         }
         return isMatch
           ? [
@@ -82,6 +96,7 @@ export default function RoutesSelection({ flags }: SelectionConfig): Handler {
       },
       [{}, {}] as [Record<string, Handler>, Record<string, string>]
     );
+    // compose the routes together
     const resolve = context.configResolver!.resolve.bind(
       context.configResolver!
     );
@@ -98,16 +113,20 @@ export default function RoutesSelection({ flags }: SelectionConfig): Handler {
         routerPromises.push(Promise.resolve([route, resolvedOrPromise]));
       }
     }
+    // build the router from entries
     const builtRoutes = Object.fromEntries(
       (await Promise.all(routerPromises)).sort(([routeString]) =>
         rankRoute(routeString)
       )
     );
     const server = router(builtRoutes);
+
+    // call the target handler
     const resp = await server(req, connInfo);
-    // set cookie
-    if (newFlags.length > 0) {
-      cookies.setFlags(resp.headers, newFlags);
+
+    // set cookie for the flags that has changed.
+    if (flagsThatShouldBeCookied.length > 0) {
+      cookies.setFlags(resp.headers, flagsThatShouldBeCookied);
     }
     return resp;
   };
