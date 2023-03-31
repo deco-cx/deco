e316b1e7532fab1430506595df6aeb603043cb38
Marcos Candeia <marrcooos@gmail.com>
2023-03-31 09:34:21 -0300

    Fix PageInclude error when section is null
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- blocks/matcher.ts
+++ blocks/matcher.ts
@@ -1,6 +1,4 @@
-import JsonViewer from "$live/components/JsonViewer.tsx";
 import { Block, InstanceOf } from "$live/engine/block.ts";
-import { context } from "$live/live.ts";
 
 export type Matcher = InstanceOf<typeof matcherBlock, "#/root/matchers">;
 
@@ -35,18 +33,6 @@ const matcherBlock: Block<MatchFunc, (ctx: MatchContext) => boolean> = {
         return matcherFuncOrValue;
       };
     },
-  defaultPreview: (matcher, { request }) => {
-    const ctx: MatchContext = { request, siteId: context.siteId };
-    return {
-      Component: JsonViewer,
-      props: {
-        body: JSON.stringify({
-          context: ctx,
-          result: matcher(ctx),
-        }),
-      },
-    };
-  },
 };
 
 /**
