dbd0c97f27fd3e8c595b5d29cf88c1556df7abe4
Marcos Candeia <marrcooos@gmail.com>
2023-04-06 08:40:42 -0300

    Fix ns on cloned https
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- utils/namespace.ts
+++ utils/namespace.ts
@@ -2,14 +2,16 @@ import { join } from "https://deno.land/std@0.170.0/path/mod.ts";
 import $ from "https://deno.land/x/dax@0.28.0/mod.ts";
 
 const namespaceFromGit = async (): Promise<string | undefined> => {
-  const lns = await $`git remote show origin -n`.lines();
+  const lns = await $`git config --get remote.origin.url`.lines();
   if (lns.length < 1) {
     return undefined;
   }
-  const fetchUrlLine = lns[1];
-  const [_ignoreFetchUrl, _ignoreGitUrl, nsAndGit] = fetchUrlLine.split(":");
-  const [namespace] = nsAndGit.split(".");
-  return namespace.trimEnd();
+  const fetchUrlLine = lns[0];
+  if (fetchUrlLine.startsWith("http")) { // http clone
+    const fetchUrl = new URL(fetchUrlLine);
+    return fetchUrl.pathname.substring(1).replace(".git", "").trimEnd(); // remove .git
+  }
+  return fetchUrlLine.replace(":", "/").trimEnd();
 };
 
 export const namespaceFromImportMap = async (
