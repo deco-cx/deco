9c5576536ffd01b13f51045c8fe1acc0dcb84c3d
Marcos Candeia <marrcooos@gmail.com>
2023-04-07 18:49:02 -0300

    Export props loader
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- mod.ts
+++ mod.ts
@@ -3,3 +3,4 @@ export * from "$live/types.ts";
 export { default as dev } from "$live/dev.ts";
 export { $live } from "$live/engine/fresh/manifest.ts";
 export { stylesPlugin };
+export type { PropsLoader } from "$live/blocks/propsResolver.ts";
