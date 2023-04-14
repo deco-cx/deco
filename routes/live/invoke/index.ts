5071dfdc0d569909b3264bc804cd73e69220e539
Marcos Candeia <marrcooos@gmail.com>
2023-04-14 09:43:06 -0300

    Fix invoke typings to support selector fields (#161)
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- routes/live/invoke/index.ts
+++ routes/live/invoke/index.ts
@@ -6,6 +6,7 @@ import { LiveConfig } from "$live/mod.ts";
 import type { DecoManifest, LiveState } from "$live/types.ts";
 import { bodyFromUrl } from "$live/utils/http.ts";
 import { DeepPick, DotNestedKeys } from "$live/utils/object.ts";
+import { UnionToIntersection } from "https://esm.sh/utility-types";
 
 export type AvailableFunctions<TManifest extends DecoManifest> =
   & keyof TManifest["functions"]
@@ -37,11 +38,15 @@ export type ManifestLoader<
   : never
   : never;
 
+export type ManifestInvocable<TManifest extends DecoManifest, TKey> =
+  TKey extends AvailableLoaders<TManifest> ? ManifestLoader<TManifest, TKey>
+    : TKey extends AvailableFunctions<TManifest>
+      ? ManifestFunction<TManifest, TKey>
+    : never;
+
 export interface InvokeFunction<
   TManifest extends DecoManifest = DecoManifest,
-  TLoader extends keyof TManifest["functions"] & string =
-    & keyof TManifest["functions"]
-    & string,
+  TLoader extends AvailableFunctions<TManifest> = AvailableFunctions<TManifest>,
   TFunc extends ManifestFunction<TManifest, TLoader> = ManifestFunction<
     TManifest,
     TLoader
@@ -55,11 +60,29 @@ export interface InvokeFunction<
   select?: TSelector[];
 }
 
+export type Invoke<
+  TManifest extends DecoManifest,
+  TInvocableKey
+    extends (AvailableFunctions<TManifest> | AvailableLoaders<TManifest>),
+  TFuncSelector extends TInvocableKey extends AvailableFunctions<TManifest>
+    ? DotNestedKeys<ManifestFunction<TManifest, TInvocableKey>["return"]>
+    : DotNestedKeys<ManifestLoader<TManifest, TInvocableKey>["return"]>,
+> = TInvocableKey extends AvailableFunctions<TManifest> ? InvokeFunction<
+    TManifest,
+    TInvocableKey,
+    ManifestFunction<TManifest, TInvocableKey>,
+    TFuncSelector
+  >
+  : InvokeLoader<
+    TManifest,
+    TInvocableKey,
+    ManifestLoader<TManifest, TInvocableKey>,
+    TFuncSelector
+  >;
+
 export interface InvokeLoader<
   TManifest extends DecoManifest = DecoManifest,
-  TLoader extends keyof TManifest["loaders"] & string =
-    & keyof TManifest["loaders"]
-    & string,
+  TLoader extends AvailableLoaders<TManifest> = AvailableLoaders<TManifest>,
   TFunc extends ManifestLoader<TManifest, TLoader> = ManifestLoader<
     TManifest,
     TLoader
@@ -86,37 +109,36 @@ export type InvokePayload<
       | Record<string, InvokeFunction<TManifest, TLoader>>
   : unknown;
 
-type ReturnWith<TRet, TPayload> = TPayload extends
-  { select: (infer Selector)[] }
-  ? Selector extends DotNestedKeys<TRet> ? DeepPick<TRet, Selector>
-  : Partial<TRet>
-  : TRet;
+type ReturnWith<TRet, TPayload> = UnionToIntersection<
+  TPayload extends { select: (infer Selector)[] }
+    ? Selector extends DotNestedKeys<TRet> ? DeepPick<TRet, Selector>
+    : Partial<TRet>
+    : TRet
+>;
+
+export type DotNestedReturn<TManifest extends DecoManifest, TInvocableKey> =
+  TInvocableKey extends AvailableFunctions<TManifest>
+    ? DotNestedKeys<ManifestFunction<TManifest, TInvocableKey>["return"]>
+    : TInvocableKey extends AvailableLoaders<TManifest>
+      ? DotNestedKeys<ManifestLoader<TManifest, TInvocableKey>["return"]>
+    : never;
 
 export type InvokeResult<
   TPayload extends
-    | InvokeFunction<TManifest>
-    | InvokeLoader<TManifest>
+    | Invoke<TManifest, any, any>
     | Record<
       string,
-      | InvokeFunction<TManifest>
-      | InvokeLoader<TManifest>
+      Invoke<TManifest, any, any>
     >,
   TManifest extends DecoManifest = DecoManifest,
-> = TPayload extends InvokeFunction<TManifest, infer TFunc, any, any>
-  ? ReturnWith<ManifestFunction<TManifest, TFunc>["return"], TPayload>
-  : TPayload extends InvokeLoader<TManifest, infer TLoader, any, any>
-    ? ReturnWith<ManifestLoader<TManifest, TLoader>["return"], TPayload>
+> = TPayload extends Invoke<TManifest, infer TFunc, any>
+  ? ReturnWith<ManifestInvocable<TManifest, TFunc>["return"], TPayload>
   : TPayload extends Record<string, any> ? {
       [key in keyof TPayload]: TPayload[key] extends
-        InvokeFunction<TManifest, infer TFunc, any, any> ? ReturnWith<
-          ManifestFunction<TManifest, TFunc>["return"],
+        Invoke<TManifest, infer TFunc, any> ? ReturnWith<
+          ManifestInvocable<TManifest, TFunc>["return"],
           TPayload[key]
         >
-        : TPayload[key] extends InvokeLoader<TManifest, infer TLoader, any, any>
-          ? ReturnWith<
-            ManifestLoader<TManifest, TLoader>["return"],
-            TPayload[key]
-          >
         : unknown;
     }
   : unknown;
--- utils/object.test.ts
+++ utils/object.test.ts
@@ -54,4 +54,19 @@ Deno.test("pickpaths", async (t) => {
     assertEquals((result.a.b as Record<string, unknown>).h, undefined);
     assertArrayIncludes(result.k.y, [{ z: 2 }, { z: 3 }]);
   });
+
+  await t.step("should pick array", () => {
+    type OtherObj = (typeof testObj)[];
+    const arrTest: OtherObj = [testObj, testObj];
+    const [result, result2] = pickPaths(arrTest, [
+      "a.b.c" as const,
+      "k.y.z" as const,
+    ]);
+    assertEquals(result.a.b.c, testObj.a.b.c);
+    assertEquals((result.a.b as Record<string, unknown>).h, undefined);
+    assertArrayIncludes(result.k.y, [{ z: 2 }, { z: 3 }]);
+    assertEquals(result2.a.b.c, testObj.a.b.c);
+    assertEquals((result2.a.b as Record<string, unknown>).h, undefined);
+    assertArrayIncludes(result2.k.y, [{ z: 2 }, { z: 3 }]);
+  });
 });
