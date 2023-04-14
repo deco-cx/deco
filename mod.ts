37019a93f193182ebc33aca58e1b7c847e9a77ca
Marcos Candeia <marrcooos@gmail.com>
2023-04-13 21:53:03 -0300

    Export with manifest
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- mod.ts
+++ mod.ts
@@ -1,7 +1,8 @@
 import stylesPlugin from "$live/plugins/styles.ts";
-export * from "$live/types.ts";
-export { default as dev } from "$live/dev.ts";
-export { $live } from "$live/engine/fresh/manifest.ts";
-export { stylesPlugin };
 export type { PropsLoader } from "$live/blocks/propsLoader.ts";
 export type { SectionProps } from "$live/blocks/section.ts";
+export { withManifest } from "$live/clients/withManifest.ts";
+export { default as dev } from "$live/dev.ts";
+export { $live } from "$live/engine/fresh/manifest.ts";
+export * from "$live/types.ts";
+export { stylesPlugin };
