9605f22cc5b991a5a4179946b6f4e7dbf7efd5bb
Marcos Candeia <marrcooos@gmail.com>
2023-05-02 18:23:47 -0300

    Use site.json to get namespace
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- dev.ts
+++ dev.ts
@@ -11,9 +11,7 @@ import { decoManifestBuilder } from "$live/engine/fresh/manifestGen.ts";
 import { genSchemasFromManifest } from "$live/engine/schema/gen.ts";
 import { context } from "$live/live.ts";
 import { DecoManifest } from "$live/types.ts";
-import { exists } from "$live/utils/filesystem.ts";
-import { namespaceFromImportMap } from "$live/utils/namespace.ts";
-import { SiteInfo } from "./types.ts";
+import { namespaceFromSiteJson, updateImportMap } from "./utils/namespace.ts";
 import { checkUpdates } from "./utils/update.ts";
 
 const schemaFile = "schemas.gen.json";
@@ -97,27 +95,8 @@ export const siteJSON = "site.json";
 const getAndUpdateNamespace = async (
   dir: string,
 ): Promise<string | undefined> => {
-  const ns = await namespaceFromImportMap(dir);
-  if (!ns) {
-    return undefined;
-  }
-  const siteJSONPath = join(dir, siteJSON);
-  let siteInfo: SiteInfo | null = null;
-  if (await exists(siteJSONPath)) {
-    siteInfo = await Deno.readTextFile(siteJSONPath).then(
-      JSON.parse,
-    );
-  } else {
-    siteInfo = {
-      namespace: ns,
-    };
-  }
-  if (siteInfo?.namespace !== ns) {
-    await Deno.writeTextFile(
-      siteJSONPath,
-      JSON.stringify({ ...siteInfo, namespace: ns }, null, 2),
-    );
-  }
+  const ns = await namespaceFromSiteJson(dir);
+  ns && await updateImportMap(dir, ns);
   return ns;
 };
 
--- utils/namespace.ts
+++ utils/namespace.ts
@@ -1,77 +1,52 @@
 import { join } from "https://deno.land/std@0.170.0/path/mod.ts";
-import $ from "https://deno.land/x/dax@0.28.0/mod.ts";
-
-const namespaceFromGit = async (): Promise<string | undefined> => {
-  const lns = await $`git config --get remote.origin.url`.lines();
-  if (lns.length < 1) {
-    return undefined;
-  }
-  const fetchUrlLine = lns[0];
-  if (fetchUrlLine.startsWith("http")) { // http clone
-    const fetchUrl = new URL(fetchUrlLine);
-    return fetchUrl.pathname.substring(1).replace(".git", "").trimEnd(); // remove .git
-  }
-  if (fetchUrlLine.startsWith("git")) {
-    const [_ignoreGitUrl, nsAndGit] = fetchUrlLine.split(":");
-    const [namespace] = nsAndGit.split(".");
-    return namespace.trimEnd();
-  }
-  return fetchUrlLine.replace(":", "/").trimEnd();
-};
+import { siteJSON } from "../dev.ts";
+import { exists } from "./filesystem.ts";
 
 const sanitize = (str: string | undefined) =>
   str?.endsWith("/") ? str : `${str}/`;
-export const namespaceFromImportMap = async (
-  dir: string,
-): Promise<string | undefined> => {
+
+export const updateImportMap = async (dir: string, ns: string) => {
+  const namespace = sanitize(ns);
   try {
-    // get from git or undefined if not available
-    let namespace = sanitize(await namespaceFromGit());
-    try {
-      // try find import map file.
-      const denoJSON = JSON.parse(
-        await Deno.readTextFile(join(dir, "deno.json")),
-      );
-      const importMapRelPath = denoJSON["importMap"] ?? "./import_map.json";
-      const importMapPath = join(dir, importMapRelPath.replace("./", ""));
-      const importMap = JSON.parse(
-        await Deno.readTextFile(importMapPath),
-      );
-      let hasNamespaceOnImportMap = false;
-      for (
-        const [maybeNamespace, importStr] of Object.entries(
-          importMap?.["imports"] ?? {},
-        )
-      ) {
-        if (importStr === "./") {
-          namespace ??= maybeNamespace;
-        }
-        hasNamespaceOnImportMap ||= maybeNamespace === namespace;
-      }
-      // if theres any namespace add on importMap
-      if (!hasNamespaceOnImportMap && namespace) {
-        const newImportMapImports = importMap?.["imports"] ?? {};
-        await Deno.writeTextFile(
-          importMapPath,
-          JSON.stringify(
-            {
-              ...(importMap ?? {}),
-              imports: {
-                [namespace]: "./",
-                ...newImportMapImports,
-              },
+    // try find import map file.
+    const denoJSON = JSON.parse(
+      await Deno.readTextFile(join(dir, "deno.json")),
+    );
+    const importMapRelPath = denoJSON["importMap"] ?? "./import_map.json";
+    const importMapPath = join(dir, importMapRelPath.replace("./", ""));
+    const importMap = JSON.parse(
+      await Deno.readTextFile(importMapPath),
+    );
+    const imports = importMap?.["imports"] ?? {};
+    // if theres any namespace add on importMap
+    if (!imports[namespace]) {
+      await Deno.writeTextFile(
+        importMapPath,
+        JSON.stringify(
+          {
+            ...(importMap ?? {}),
+            imports: {
+              [namespace]: "./",
+              ...imports,
             },
-            null,
-            2,
-          ),
-        );
-      }
-    } catch (err) {
-      console.warn("could not read importmap", err);
+          },
+          null,
+          2,
+        ),
+      );
     }
-    return namespace && namespace.substring(0, namespace.length - 1); // removing /
   } catch (err) {
-    console.error("could not retrieve namespace error", err);
-    return undefined;
+    console.warn("could not read importmap", err);
   }
+};
+
+export const namespaceFromSiteJson = async (dir: string) => {
+  const siteJSONPath = join(dir, siteJSON);
+  if (await exists(siteJSONPath)) {
+    const siteInfo = await Deno.readTextFile(siteJSONPath).then(
+      JSON.parse,
+    );
+    return siteInfo?.namespace;
+  }
+  return undefined;
 };
