79610da7f0421e01ceae60b8706978cd477bc254
Marcos Candeia <marrcooos@gmail.com>
2023-05-06 19:56:05 -0300

    Add retry with a new key in case of not match signature (supporting rotating keys)
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- actions/workflows/run.ts
+++ actions/workflows/run.ts
@@ -17,6 +17,20 @@ const getOrFetchPublicKey = (): Promise<JsonWebKey> => {
   const [_, serviceUrl] = workflowServiceInfo();
   return key ??= fetchPublicKey(serviceUrl);
 };
+
+const verifyWithCurrentKeyOrRefetch = async (req: Request) => {
+  try {
+    verifySignature(req, await getOrFetchPublicKey());
+  } catch (err) {
+    console.log(
+      "error when validating signature",
+      err,
+      "retrying with a new key",
+    );
+    key = null;
+    verifySignature(req, await getOrFetchPublicKey());
+  }
+};
 /**
  * @description Proceed the workflow execution based on the current state of the workflow.
  */
@@ -24,7 +38,7 @@ export default async function runWorkflow(
   props: Props,
   req: Request,
 ): Promise<Command> {
-  verifySignature(req, await getOrFetchPublicKey());
+  await verifyWithCurrentKeyOrRefetch(req);
   const { metadata: { workflow } } = props;
   const handler = workflowRemoteRunner(workflow, WorkflowContext);
   return handler(props);
