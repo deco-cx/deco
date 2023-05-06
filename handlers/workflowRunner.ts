34af3ad49cf4335a8e21a58c0ec9d0beed62b05b
Marcos Candeia <marrcooos@gmail.com>
2023-05-06 19:48:41 -0300

    Add fixme on workflow runner handler
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- handlers/workflowRunner.ts
+++ handlers/workflowRunner.ts
@@ -1,10 +1,12 @@
 import { workflowHTTPHandler } from "$live/deps.ts";
 import { Workflow, WorkflowContext } from "$live/blocks/workflow.ts";
+import { Handler } from "../blocks/handler.ts";
 export interface Config {
   workflow: Workflow;
 }
 
-export default function WorkflowHandler({ workflow }: Config) {
+export default function WorkflowHandler({ workflow }: Config): Handler {
+  // FIXME missing verify request signature here.
   const handler = workflowHTTPHandler(workflow, WorkflowContext);
   return handler;
 }
