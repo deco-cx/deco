8236504ff9f2e70c7d18165c2fb9c7be240add50
Marcos Candeia <marrcooos@gmail.com>
2023-05-06 19:47:02 -0300

    Verify signature on run.ts
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- actions/workflows/run.ts
+++ actions/workflows/run.ts
@@ -1,13 +1,31 @@
 import { Workflow, WorkflowContext } from "$live/blocks/workflow.ts";
-import { Arg, Command, RunRequest, workflowRemoteRunner } from "$live/deps.ts";
+import {
+  Arg,
+  Command,
+  fetchPublicKey,
+  RunRequest,
+  verifySignature,
+  workflowRemoteRunner,
+} from "$live/deps.ts";
+import { workflowServiceInfo } from "../../commons/workflows/serviceInfo.ts";
 
 export type Props = RunRequest<Arg, { workflow: Workflow }>;
+
+let key: Promise<JsonWebKey> | null = null;
+
+const getOrFetchPublicKey = (): Promise<JsonWebKey> => {
+  const [_, serviceUrl] = workflowServiceInfo();
+  return key ??= fetchPublicKey(serviceUrl);
+};
 /**
  * @description Proceed the workflow execution based on the current state of the workflow.
  */
-export default function runWorkflow(
+export default async function runWorkflow(
   props: Props,
-): Command {
+  req: Request,
+): Promise<Command> {
+  console.log(await getOrFetchPublicKey());
+  verifySignature(req, await getOrFetchPublicKey());
   const { metadata: { workflow } } = props;
   const handler = workflowRemoteRunner(workflow, WorkflowContext);
   return handler(props);
