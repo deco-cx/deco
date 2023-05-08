8a0c67943679b812d8ef083c259d6a2e9c480e44
Marcos Candeia <marrcooos@gmail.com>
2023-05-08 16:59:29 -0300

    Use host when origin is not provided
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- matchers/MatchOrigin.ts
+++ matchers/MatchOrigin.ts
@@ -9,9 +9,8 @@ const MatchOrigin = (
   { includes, match }: Props,
   { request }: MatchContext,
 ) => {
-  console.log("HOST", request.headers.get("host"));
-  const origin = request.headers.get("origin") || "";
-  console.log("ORIGIN", origin);
+  const origin = request.headers.get("origin") ?? request.headers.get("host") ??
+    "";
   const regexMatch = match ? new RegExp(match).test(origin) : true;
   const includesFound = includes ? origin.includes(includes) : true;
 
