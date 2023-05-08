2a71f52f539a5910350534d442afa2441b1b18a0
Marcos Candeia <marrcooos@gmail.com>
2023-05-08 17:02:00 -0300

    Use || operator instead of ??
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- matchers/MatchOrigin.ts
+++ matchers/MatchOrigin.ts
@@ -9,7 +9,7 @@ const MatchOrigin = (
   { includes, match }: Props,
   { request }: MatchContext,
 ) => {
-  const origin = request.headers.get("origin") ?? request.headers.get("host") ??
+  const origin = request.headers.get("origin") || request.headers.get("host") ||
     "";
   const regexMatch = match ? new RegExp(match).test(origin) : true;
   const includesFound = includes ? origin.includes(includes) : true;
