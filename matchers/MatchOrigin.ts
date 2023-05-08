1cebcb76cc2465bc69c9b54ca3507d3049bb59e4
Marcos Candeia <marrcooos@gmail.com>
2023-05-08 17:10:12 -0300

    Remove logs
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- matchers/MatchOrigin.ts
+++ matchers/MatchOrigin.ts
@@ -11,12 +11,8 @@ const MatchOrigin = (
 ) => {
   const origin = request.headers.get("origin") || request.headers.get("host") ||
     "";
-  console.log("ORIGIN", origin);
   const regexMatch = match ? new RegExp(match).test(origin) : true;
-  console.log("REGEX MATCH", regexMatch);
   const includesFound = includes ? origin.includes(includes) : true;
-
-  console.log("INCLUDES", includes, includesFound, origin.includes(includes));
 
   return regexMatch && includesFound;
 };
