7d3c2101462a8a331c9dabbbbdc807b495d28dd2
Marcos Candeia <marrcooos@gmail.com>
2023-05-08 13:02:18 -0300

    Update durable ver
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 export { DomInspectorActivators } from "https://deno.land/x/inspect_vscode@0.2.1/inspector.ts";
 export * as inspectVSCode from "https://deno.land/x/inspect_vscode@0.2.1/mod.ts";
-export * from "https://denopkg.com/deco-cx/durable@d9e401af2ffdd4b535539eba8836ccda22d6d926/mod.ts";
+export * from "https://denopkg.com/deco-cx/durable@4b671cba324c63728cc8a24b2d39cc7e2402a84c/mod.ts";
 export * as supabase from "https://esm.sh/@supabase/supabase-js@2.7.0";
 export type {
   JSONSchema7,
