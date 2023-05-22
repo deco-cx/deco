a7cf79cb92dc637eb10ebd3273abe9f4cdcd4f31
Marcos Candeia <marrcooos@gmail.com>
2023-05-22 10:55:43 -0300

    Back resolve depth to 2
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- engine/core/resolver.ts
+++ engine/core/resolver.ts
@@ -280,7 +280,7 @@ export const withResolveChain = <T extends BaseContext = BaseContext>(
   };
 };
 
-const MAX_DEPTH_RESOLVE = 3;
+const MAX_DEPTH_RESOLVE = 2;
 export const resolve = async <
   T,
   TContext extends BaseContext = BaseContext,
