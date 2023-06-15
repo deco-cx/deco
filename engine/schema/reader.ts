82a6fab6141b7f583d4a4f9e156c6a7143ced981
Marcos Candeia <marrcooos@gmail.com>
2023-06-15 07:57:35 -0300

    Revert "Runtime schema gen (#299)"
    This reverts commit d60fddb7d8cdf50ef68be2f331cc017cd2fd3e6c.

--- engine/schema/reader.ts
+++ engine/schema/reader.ts
@@ -1,14 +1,25 @@
 import { Schemas } from "$live/engine/schema/builder.ts";
 import { context } from "$live/live.ts";
+import { join } from "https://deno.land/std@0.61.0/path/mod.ts";
 import { genSchemasFromManifest } from "./gen.ts";
 
 let schemas: Promise<Schemas> | null = null;
+const schemaFile = "schemas.gen.json";
 
 export const genSchemas = async () => {
   console.log(`🌟 live.ts is spinning up some magic for you! ✨ Hold tight!`);
   const start = performance.now();
   const schema = await genSchemasFromManifest(
     context.manifest!,
+  );
+
+  await Deno.writeTextFile(
+    join(Deno.cwd(), schemaFile),
+    JSON.stringify(
+      schema,
+      null,
+      2,
+    ),
   );
 
   console.log(
@@ -22,6 +33,14 @@ export const genSchemas = async () => {
   return schema;
 };
 
+const getSchema = async (): Promise<Schemas> => {
+  return await Deno.readTextFile(join(Deno.cwd(), schemaFile)).then(JSON.parse);
+};
+
 export const getCurrent = (): Promise<Schemas> => {
-  return schemas ??= genSchemas();
+  return schemas ??= context.isDeploy ? getSchema() : genSchemas();
+};
+
+export const reset = () => {
+  schemas = null;
 };
