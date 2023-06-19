716e62520b571896bf832bf1a90059983bc3bb52
Marcos Candeia <marrcooos@gmail.com>
2023-06-19 20:14:36 -0300

    Add match date with cron
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- matchers/MatchDate.ts
+++ matchers/MatchDate.ts
@@ -1,4 +1,9 @@
-export interface Props {
+import Cron from "https://deno.land/x/croner@6.0.3/dist/croner.js";
+
+/**
+ * @titleBy start
+ */
+export interface DateProps {
   /**
    * @format date-time
    */
@@ -9,11 +14,55 @@ export interface Props {
   end?: string;
 }
 
+/**
+ * @titleBy cron
+ */
+export interface CronProps {
+  /**
+   * @format cron
+   */
+  cron: string;
+}
+
+const isDateProps = (props: Props): props is DateProps => {
+  return (props as CronProps)?.cron === undefined;
+};
+export type Props = CronProps | DateProps;
+
+const addMSToDate = (date: Date, ms: number) => {
+  const currentTimeAsMs = date.getTime();
+
+  const adjustedTimeAsMs = currentTimeAsMs + ms;
+
+  const adjustedDateObj = new Date(adjustedTimeAsMs);
+  return adjustedDateObj;
+};
+
+const ONE_MINUTE_MS = 60_000;
+
+function nowWithMinutePrecision() {
+  const date = new Date();
+  date.setSeconds(0);
+  date.setMilliseconds(0);
+  date.setTime(Math.floor(date.getTime() / 1000) * 1000);
+  return date;
+}
+
+/**
+ * @title Match Date.
+ */
 const MatchDate = (props: Props) => {
   const now = new Date();
-  const start = props.start ? now > new Date(props.start) : true;
-  const end = props.end ? now < new Date(props.end) : true;
-  return start && end;
+  if (isDateProps(props)) {
+    const start = props.start ? now > new Date(props.start) : true;
+    const end = props.end ? now < new Date(props.end) : true;
+    return start && end;
+  }
+  const minutePrecision = nowWithMinutePrecision(); // cron jobs has only minutes precision
+
+  const cron = new Cron(props.cron);
+  return cron.nextRun(addMSToDate(minutePrecision, -ONE_MINUTE_MS))
+    ?.getTime() === minutePrecision.getTime();
 };
 
 export default MatchDate;
