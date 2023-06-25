7abf29bd3ea5318db24d1648b97751e32c963b80
Guilherme Rodrigues <gui@deco.cx>
2023-06-25 07:24:04 -0300

    Another one

--- routes/live/inspect/[...block].ts
+++ routes/live/inspect/[...block].ts
@@ -1,5 +1,5 @@
 import { context } from "$live/live.ts";
-import { inspectHandler } from "https://deno.land/x/inspect_vscode@0.2.1/mod.ts";
+import { inspectHandler } from "https://cdn.jsdelivr.net/gh/deco-cx/inspect-vscode@0.2.1/mod.ts";
 
 export const handler = (req: Request) => {
   return inspectHandler(`/live/inspect/${context.namespace}`, req);
