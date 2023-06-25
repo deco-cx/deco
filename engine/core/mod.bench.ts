8281ef08afdd9972124d867865abd95c0b17d262
Marcos Candeia <marrcooos@gmail.com>
2023-06-25 08:23:25 -0300

    Add pre-generated hints benchcase
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- engine/core/mod.bench.ts
+++ engine/core/mod.bench.ts
@@ -1,10 +1,12 @@
+import { genHints } from "$live/engine/core/hints.ts";
 import { BaseContext, resolve } from "$live/engine/core/resolver.ts";
 import meta from "$live/meta.json" assert { type: "json" };
 import releaseJSON from "./hints.test.json" assert { type: "json" };
 const danglingRecover = (parent: unknown) => parent;
+
 const resolveHints = {}; //on-demand hints
 Deno.bench(
-  "resolve current version",
+  "resolve current version (with on-demand hints)",
   { group: "resolve", baseline: true },
   async () => {
     const context: BaseContext = {
@@ -13,6 +15,34 @@ Deno.bench(
       resolvables: releaseJSON,
       resolvers: {},
       resolveHints,
+      danglingRecover,
+      resolve: <T>(data: unknown) => {
+        return data as T;
+      },
+    };
+
+    const waitAll: Promise<unknown>[] = [];
+    Object.keys(releaseJSON).map((key) => {
+      waitAll.push(resolve(
+        key,
+        context,
+      ));
+    });
+    await Promise.all(waitAll);
+  },
+);
+
+const generatedHints = genHints(releaseJSON);
+Deno.bench(
+  "resolve current version (with pregenerated hints)",
+  { group: "resolve" },
+  async () => {
+    const context: BaseContext = {
+      resolveChain: [],
+      resolveId: "1",
+      resolvables: releaseJSON,
+      resolvers: {},
+      resolveHints: generatedHints,
       danglingRecover,
       resolve: <T>(data: unknown) => {
         return data as T;
