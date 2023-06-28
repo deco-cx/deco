e05f171735ffd8b3fe4190ff4efc63be2b3820f2
Marcos Candeia <marrcooos@gmail.com>
2023-06-28 20:10:52 -0300

    Support redirect params
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- handlers/redirect.ts
+++ handlers/redirect.ts
@@ -1,3 +1,6 @@
+import { ConnInfo } from "std/http/mod.ts";
+import { isFreshCtx } from "./fresh.ts";
+
 export interface RedirectConfig {
   to: string;
   type?: "permanent" | "temporary";
@@ -13,11 +16,14 @@ export default function Redirect({ to, type = "temporary" }: RedirectConfig) {
     "permanent": 301,
   };
 
-  return () => {
+  return (_req: Request, conn: ConnInfo) => {
+    const params = isFreshCtx(conn) ? conn.params ?? {} : {};
+    const location = to.replace(/:[^\/]+/g, (g) => (params[g.substr(1)]));
+
     return new Response(null, {
       status: statusByRedirectType[type],
       headers: {
-        location: to,
+        location,
       },
     });
   };
