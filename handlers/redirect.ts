f723667210e0eca55c258cf7eeb9a79cfaa157df
Marcos Candeia <marrcooos@gmail.com>
2023-06-29 07:37:51 -0300

    Support redirect params (#326)
    * Support redirect params
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Do not replace to when params has length 0
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Remove deno doc dependency from deps.ts
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    ---------
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- handlers/redirect.ts
+++ handlers/redirect.ts
@@ -1,3 +1,6 @@
+import { ConnInfo } from "std/http/mod.ts";
+import { isFreshCtx } from "./fresh.ts";
+
 export interface RedirectConfig {
   to: string;
   type?: "permanent" | "temporary";
@@ -13,11 +16,16 @@ export default function Redirect({ to, type = "temporary" }: RedirectConfig) {
     "permanent": 301,
   };
 
-  return () => {
+  return (_req: Request, conn: ConnInfo) => {
+    const params = isFreshCtx(conn) ? conn.params ?? {} : {};
+    const location = Object.keys(params).length > 0
+      ? to.replace(/:[^\/]+/g, (g) => (params[g.substr(1)]))
+      : to;
+
     return new Response(null, {
       status: statusByRedirectType[type],
       headers: {
-        location: to,
+        location,
       },
     });
   };
