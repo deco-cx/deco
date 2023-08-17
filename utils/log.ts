2f2b8af8fc51d857f3a24c3c90437446a91cfdaa
Lucis <luciannojunior@gmail.com>
2023-08-17 16:10:48 -0300

    Improving logs and loggins outgoing requests

--- utils/log.ts
+++ utils/log.ts
@@ -1,11 +1,49 @@
 import {
   blue,
+  bold,
   cyan,
+  gray,
   green,
   red,
   yellow,
 } from "https://deno.land/std@0.147.0/fmt/colors.ts";
 import { context } from "$live/live.ts";
+
+export const formatHeaders = (h: Headers) => {
+  const headersAsObject: Record<string, string> = {};
+  for (const [key, value] of h.entries()) {
+    headersAsObject[key] = value;
+  }
+
+  return `${cyan(bold("Incoming Headers:"))} ${
+    JSON.stringify(headersAsObject)
+  }\n`;
+};
+
+export const formatIncomingRequest = (request: Request, site: string) => {
+  return `${cyan(bold("Site: "))}${site} - ${
+    cyan(bold("URL:"))
+  } ${request.url}\n${formatHeaders(request.headers)}`;
+};
+
+export const formatOutgoingFetch = (
+  input: string | Request | URL,
+  init?: RequestInit | undefined,
+) => {
+  const method = (input instanceof Request ? input.method : init?.method) ??
+    "GET";
+  const url = (() => {
+    if (input instanceof URL) {
+      return input.href;
+    }
+    if (input instanceof Request) {
+      return input.url;
+    }
+    return input;
+  })();
+
+  return ` -> ${bold(method)} | ${gray(url)}`;
+};
 
 export const formatLog = (opts: {
   status: number;
