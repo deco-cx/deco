b78138e8a76e388f3bcf31811e83047692809e16
Marcos Candeia <marrcooos@gmail.com>
2023-08-20 08:18:45 -0300

    Export extra types
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- mod.ts
+++ mod.ts
@@ -1,6 +1,7 @@
 import stylesPlugin from "./plugins/styles.ts";
-export type { AppManifest, Apps } from "./blocks/app.ts";
+export type { AppManifest, Apps, ManifestOf } from "./blocks/app.ts";
 export type { Handler } from "./blocks/handler.ts";
+export * as blocks from "./blocks/index.ts";
 export { WorkflowContext } from "./blocks/workflow.ts";
 export type { Workflow } from "./blocks/workflow.ts";
 export { withManifest } from "./clients/withManifest.ts";
@@ -12,10 +13,10 @@ export { badRequest, notFound, redirect } from "./engine/errors.ts";
 export { $live } from "./engine/fresh/manifest.ts";
 export type { Route, Routes } from "./flags/audience.ts";
 export { context } from "./live.ts";
+export type { Secret } from "./loaders/secret.ts";
 export * from "./types.ts";
 export type { App, AppContext, AppModule, AppRuntime } from "./types.ts";
+export { allowCorsFor } from "./utils/http.ts";
 export type { StreamProps } from "./utils/invoke.ts";
 export { stylesPlugin };
-export { allowCorsFor } from "./utils/http.ts";
-export type { Secret } from "./loaders/secret.ts";
-export * as blocks from "./blocks/index.ts";
+
