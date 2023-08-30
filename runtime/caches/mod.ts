2efbbb771f88ccd89b868035ec176885dd52274b
Lucis <luciannojunior@gmail.com>
2023-08-30 19:29:37 -0300

    Adding weird log 3

--- runtime/caches/mod.ts
+++ runtime/caches/mod.ts
@@ -5,13 +5,16 @@ const WEB_CACHE_ENGINE = Deno.env.get("WEB_CACHE_ENGINE");
 
 const getCacheStorage = (): CacheStorage => {
   if (typeof Deno.openKv === "function" && WEB_CACHE_ENGINE === "KV") {
+    console.log("Returning KV cache");
     return cachesKV;
   }
 
   if (typeof globalThis.caches !== "undefined") {
+    console.log("Returning global Cache");
     return globalThis.caches;
   }
 
+  console.log("Returning cache proxy");
   return cachesProxy;
 };
 
