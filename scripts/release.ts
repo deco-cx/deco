b68793cded43d1e00d40b9376aa17dc6ff66a7ba
Marcos Candeia <marrcooos@gmail.com>
2023-10-10 17:19:02 -0300

    Fix release
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,7 +1,7 @@
 import { Select } from "https://deno.land/x/cliffy@v0.25.5/prompt/mod.ts";
 import { exec, OutputMode } from "https://deno.land/x/exec@0.0.5/mod.ts";
 import { join } from "std/path/mod.ts";
-import { increment } from "std/semver/mod.ts";
+import { format, increment, parse } from "std/semver/mod.ts";
 import { stringifyForWrite } from "../utils/json.ts";
 
 await exec("git fetch --tags");
@@ -26,24 +26,30 @@ const response = await exec(`bash -c "git tag | sort -V | tail -1"`, {
   output: OutputMode.Capture,
 });
 
-const latestTag = response.output;
+const latestTag = parse(response.output);
 
 console.log(`Current version is: ${latestTag}`);
 
 // TODO: Only allow releases in main branch
 
-const patchIncrement = increment(latestTag, "patch") || "";
-const minorIncrement = increment(latestTag, "minor") || "";
-const majorIncrement = increment(latestTag, "major") || "";
+const patchIncrement = format(increment(latestTag, "patch"), "full") || "";
+const minorIncrement = format(increment(latestTag, "minor"), "full") || "";
+const majorIncrement = format(increment(latestTag, "major"), "full") || "";
 
 const newVersionByUser = await Select.prompt({
   options: [
-    { value: patchIncrement, name: `Patch (${patchIncrement})` },
+    {
+      value: patchIncrement,
+      name: `Patch (${patchIncrement})`,
+    },
     {
       value: minorIncrement,
       name: `Minor (${minorIncrement})`,
     },
-    { value: majorIncrement, name: `Major (${majorIncrement})` },
+    {
+      value: majorIncrement,
+      name: `Major (${majorIncrement})`,
+    },
     { value: "custom", name: "Custom" },
   ],
   message: "Select the bump you want to release: ",
