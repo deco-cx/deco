6b1ad0c053091b5c74692bc5add229fff0de5bb6
Lucis <luciannojunior@gmail.com>
2023-10-11 13:47:46 -0300

    Allow admin.deco.cx to preview

--- utils/admin.ts
+++ utils/admin.ts
@@ -1,6 +1,7 @@
 import type { JSONSchema7 } from "../deps.ts";
 
 export const adminDomain = `https://deco.cx`;
+export const adminAlternateDomain = `https://admin.deco.cx`;
 export const landingPageDomain = `https://www.deco.cx`;
 const adminPreviewUrls = "https://deco-sites-admin-";
 const adminPreviewDomain = "deno.dev";
