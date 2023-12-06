526fabecbb8c325e2bf05c8eb76866812a7d9d67
Marcos Candeia <marrcooos@gmail.com>
2023-12-06 18:28:45 -0300

    Set device on http context
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

---
+++ utils/device.ts
@@ -0,0 +1,32 @@
+import { UAParser } from "https://esm.sh/ua-parser-js@1.0.35";
+
+/**
+ * @title {{{.}}}
+ */
+export type Device = "mobile" | "tablet" | "desktop";
+
+const ideviceToDevice: Record<string, Device> = {
+  mobile: "mobile",
+  tablet: "tablet",
+  console: "desktop",
+  smarttv: "desktop",
+  wearable: "desktop",
+  embedded: "desktop",
+};
+
+export const deviceOf = (request: Request) => {
+  const url = new URL(request.url);
+  const ua: string | null = request.headers.get("user-agent") || "";
+  // use cf hint at first and then fallback to user-agent parser.
+  const cfDeviceHint: string | null = request.headers.get("cf-device-type") ||
+    "";
+
+  const device = cfDeviceHint ||
+    (ua && new UAParser(ua).getDevice().type) ||
+    url.searchParams.get("deviceHint") ||
+    "desktop"; // console, mobile, tablet, smarttv, wearable, embedded
+
+  const normalizedDevice = ideviceToDevice[device] ?? "desktop";
+
+  return normalizedDevice;
+};
