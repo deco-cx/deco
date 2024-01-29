beef87c7679faecadafe60fdd64f516e31962552
Marcos Candeia <marrcooos@gmail.com>
2024-01-29 15:35:50 -0300

    Separate alien release from contextProvider
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

---
+++ runtime/fresh/middlewares/0_contextProvider.ts
@@ -0,0 +1,61 @@
+// deno-lint-ignore-file no-explicit-any
+import { Context, DecoContext } from "deco/deco.ts";
+import { MiddlewareHandler, MiddlewareHandlerContext } from "../../../deps.ts";
+import { DECO_FILE_NAME, newFsProvider } from "../../../engine/releases/fs.ts";
+import { newContext } from "../../../mod.ts";
+import { DecoOptions, OptionsProvider } from "../../../plugins/deco.ts";
+import { AppManifest, DecoSiteState, DecoState } from "../../../types.ts";
+import { ContextCache } from "./1_alienRelease.ts";
+
+let contextCache: ContextCache | null = null;
+
+export const contextProvider = <TManifest extends AppManifest = AppManifest>(
+  _opt: DecoOptions<TManifest> | OptionsProvider,
+): MiddlewareHandler<DecoState<any, DecoSiteState, TManifest>> => {
+  // Return an async function to handle requests
+  return async function (
+    request: Request,
+    context: MiddlewareHandlerContext<
+      DecoState<any, DecoSiteState, TManifest>
+    >,
+  ) {
+    const opt = typeof _opt === "function" ? await _opt(request) : _opt;
+    const releaseProvider =
+      opt?.useLocalStorageOnly || Deno.env.has("USE_LOCAL_STORAGE_ONLY")
+        ? newFsProvider(DECO_FILE_NAME, opt.manifest.name)
+        : opt.release;
+    // Define root manifest
+    const rootManifest = {
+      baseUrl: opt.manifest.baseUrl,
+      name: opt.manifest.name,
+      apps: { ...opt.manifest.apps },
+    };
+
+    contextCache ??= new ContextCache({
+      cacheSize: 7, // 7 is arbitrarily chosen
+    });
+
+    let contextPromise: Promise<DecoContext> | undefined = contextCache.get(
+      opt,
+    );
+
+    if (!contextPromise) {
+      contextPromise = newContext(
+        rootManifest,
+        opt.sourceMap,
+        releaseProvider,
+      );
+      contextCache.set(
+        opt,
+        contextPromise,
+      );
+    }
+
+    const next = Context.bind(
+      await contextPromise,
+      context.next.bind(context),
+    );
+
+    return next();
+  };
+};
