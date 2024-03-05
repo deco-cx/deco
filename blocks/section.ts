759dcedee2e0e46009d1dbd59623aad38bd2bab1
igorbrasileiro <brasileiro456@gmail.com>
2024-03-05 17:38:19 -0300

    schema gen handle chose function return types as array

--- blocks/section.ts
+++ blocks/section.ts
@@ -105,7 +105,7 @@ export const createSectionBlock = (
   type: "sections" | "pages",
 ): Block<SectionModule> => ({
   type,
-  introspect: { funcNames: ["loader", "default"], includeReturn: true },
+  introspect: { funcNames: ["loader", "default"], includeReturn: ["default"] },
   adapt: <TConfig = any, TProps = any>(
     mod: SectionModule<TConfig, TProps>,
     resolver: string,
