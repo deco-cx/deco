b1f3c8dd5d30ca3fbf91da522a56f7ca775ad5b8
guitavano <tavano62@gmail.com>
2024-03-05 10:07:17 -0300

    remove useRequest because we have usePageContext

--- hooks/useRequest.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from "preact/hooks";
-import { SectionContext } from "deco/components/section.tsx";
-
-export const useRequest = () => {
-    const ctx = useContext(SectionContext);
-    console.log(ctx?.request)
-
-    return ctx?.request;
-}
