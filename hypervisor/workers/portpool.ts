97111fa737e031c454f6d03f51b7fd3d8a23ee39
Marcos Candeia <marrcooos@gmail.com>
2024-04-27 12:05:02 -0300

    Do not depend on external libraries
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- hypervisor/workers/portpool.ts
+++ hypervisor/workers/portpool.ts
@@ -1,10 +1,25 @@
-import {
-  type checkedPort,
-  makeRange,
-  max,
-  min,
-} from "https://deno.land/x/getport@v2.1.2/mod.ts";
+const min = 1024;
+const max = 65535;
 
+function makeRange(from: number, to: number): number[] {
+  if (!(from > min || from < max)) {
+    throw new RangeError("`from` must be between 1024 and 65535");
+  }
+  if (!(to > min || to < max)) {
+    throw new RangeError("`to` must be between 1024 and 65536");
+  }
+  if (!(to > from)) {
+    throw new RangeError("`to` must be greater than or equal to `from`");
+  }
+  const ports = [];
+  for (let port = from; port <= to; port++) ports.push(port);
+  return ports;
+}
+
+interface CheckedPort {
+  valid: boolean;
+  port: number;
+}
 class PortPool implements PortPool {
   private usedPorts: Set<number>;
   private minPort: number;
@@ -42,13 +57,13 @@ class PortPool implements PortPool {
   private generateAvailablePorts(): number[] {
     const allPorts = makeRange(this.minPort, this.maxPort);
     const availablePorts = allPorts.filter((port) => {
-      const result: checkedPort = this.checkPort({ port });
+      const result: CheckedPort = this.checkPort({ port });
       return result.valid && !this.usedPorts.has(port);
     });
     return availablePorts;
   }
 
-  private checkPort(options: Deno.ListenOptions): checkedPort {
+  private checkPort(options: Deno.ListenOptions): CheckedPort {
     const { port } = options;
     try {
       const server = Deno.listen(options);
