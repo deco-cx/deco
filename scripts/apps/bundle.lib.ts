00085afa109a33a1bf63d320452e97dbd30ce0cb
Marcos Candeia <marrcooos@gmail.com>
2024-05-10 09:25:41 -0300

    Separate bundle as a lib
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- scripts/apps/bundle.lib.ts
+++ scripts/apps/bundle.lib.ts
@@ -1,3 +1,33 @@
-import { bundleApps } from "./bundle.ts";
+import * as colors from "std/fmt/colors.ts";
+import { join } from "std/path/mod.ts";
+import { decoManifestBuilder } from "../../engine/manifest/manifestGen.ts";
+import { format } from "../../utils/formatter.ts";
+import { type AppConfig, getDecoConfig } from "./config.ts";
 
-await bundleApps();
+export const bundleApp = (dir: string) => async (app: AppConfig) => {
+  console.log(`generating manifest for ${colors.bgBrightGreen(app.name)}...`);
+  const appDir = join(dir, app.dir);
+  const manifest = await decoManifestBuilder(appDir, app.name);
+
+  const manifestFile = join(appDir, "manifest.gen.ts");
+  await Deno.writeTextFile(
+    manifestFile,
+    await format(manifest.build()),
+  );
+  console.log(
+    colors.brightBlue(`the manifest of ${app.name} has been generated`),
+  );
+};
+export const bundleApps = async () => {
+  const dir = Deno.cwd();
+  console.log(colors.brightGreen(`start bundling apps... ${dir}`));
+  const decoConfig = await getDecoConfig(dir);
+
+  console.debug(
+    `found ${
+      colors.brightBlue((decoConfig?.apps ?? []).length.toString())
+    } apps`,
+  );
+
+  await Promise.all((decoConfig?.apps ?? []).map(bundleApp(dir)));
+};
