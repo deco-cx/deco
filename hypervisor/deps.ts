8a128749a8f765cdba368ab31628293d4e7e39d7
Marcos Candeia <marrcooos@gmail.com>
2024-05-23 20:55:03 -0300

    Upgrade jsr (#689)
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- hypervisor/deps.ts
+++ hypervisor/deps.ts
@@ -1,3 +1,4 @@
-export { FSError, realtimeFor } from "jsr:@deco-cx/realtime@0.1.5";
-export type { File, RealtimeState } from "jsr:@deco-cx/realtime@0.1.5";
+export { FSError, realtimeFor } from "jsr:@deco-cx/realtime@0.1.6";
+export type { File, RealtimeState } from "jsr:@deco-cx/realtime@0.1.6";
 export * as gitIgnore from "npm:ignore@5.3.1";
+
