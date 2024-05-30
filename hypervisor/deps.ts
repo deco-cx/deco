0def45d44464a91c1a5377cd1562e785dec21f5c
Marcos Candeia <marrcooos@gmail.com>
2024-05-30 08:27:26 -0300

    Use deco realtime 0.1.0
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- hypervisor/deps.ts
+++ hypervisor/deps.ts
@@ -1,4 +1,4 @@
-export { FSError, realtimeFor } from "jsr:@deco-cx/realtime@0.1.6";
-export type { File, RealtimeState } from "jsr:@deco-cx/realtime@0.1.6";
+export { FSError, realtimeFor } from "jsr:@deco/realtime@0.1.0";
+export type { File, RealtimeState } from "jsr:@deco/realtime@0.1.0";
 export * as gitIgnore from "npm:ignore@5.3.1";
 
