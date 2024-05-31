de1ec430f827c270adc06da226212a8f0a1a955f
Marcos Candeia <marrcooos@gmail.com>
2024-05-31 20:25:19 -0300

    Upgrade warp version
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- hypervisor/deps.ts
+++ hypervisor/deps.ts
@@ -1,3 +1,3 @@
-export { FSError, realtimeFor } from "jsr:@deco/realtime@0.1.0";
-export type { File, RealtimeState } from "jsr:@deco/realtime@0.1.0";
 export * as gitIgnore from "npm:ignore@5.3.1";
+export { FSError, realtimeFor } from "../../realtime/mod.ts";
+export type { File, RealtimeState } from "../../realtime/mod.ts";
