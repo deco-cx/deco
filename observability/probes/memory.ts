c4fc63dab57e867851713830d8cf23cd706be463
Marcos Candeia <marrcooos@gmail.com>
2024-06-03 19:55:50 -0300

    Add configurable probe ratio
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- observability/probes/memory.ts
+++ observability/probes/memory.ts
@@ -1,6 +1,9 @@
 import { getProbeThresholdAsNum, type LiveChecker } from "./handler.ts";
 
-const healthyRatio = 0.95;
+const HEALTHY_RATIO = Deno.env.get("PROBE_MEMORY_HEALTHY_RATIO");
+
+const healthyRatio = HEALTHY_RATIO ? parseFloat(HEALTHY_RATIO) : 0.81;
+
 const NAME = "MEMORY";
 const MAX_MEM_THRESHOLD_MB = getProbeThresholdAsNum(NAME);
 
