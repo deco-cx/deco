89ac7e0611ce6a5981979ac35f2bc5a7c621f304
Marcos Candeia <marrcooos@gmail.com>
2024-07-25 18:32:01 -0300

    Extract dev server path
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- runtime/hono/setup.tsx
+++ runtime/hono/setup.tsx
@@ -19,127 +19,128 @@ import { join } from "std/path/join.ts";
 import { handler as metaHandler } from "./routes/_meta.ts";
 import { handler as invokeHandler } from "./routes/batchInvoke.ts";
 import {
-  default as PreviewPage,
-  handler as previewHandler,
+    default as PreviewPage,
+    handler as previewHandler,
 } from "./routes/blockPreview.tsx";
 import {
-  default as Render,
-  handler as entrypoint,
+    default as Render,
+    handler as entrypoint,
 } from "./routes/entrypoint.tsx";
 import { handler as inspectHandler } from "./routes/inspect.ts";
 import { handler as invokeKeyHandler } from "./routes/invoke.ts";
 import {
-  default as PreviewsPage,
-  handler as previewsHandler,
+    default as PreviewsPage,
+    handler as previewsHandler,
 } from "./routes/previews.tsx";
 import { handler as releaseHandler } from "./routes/release.ts";
 import { handler as renderHandler } from "./routes/render.tsx";
 import { handler as workflowHandler } from "./routes/workflow.ts";
 
+const DEV_SERVER_PATH = `/deco/dev`;
 // deno-lint-ignore no-explicit-any
 export type PageParams<TData = any> = Pick<
-  PageProps<TData>,
-  "data" | "url" | "params"
+    PageProps<TData>,
+    "data" | "url" | "params"
 >;
 let socket: null | WebSocket = null;
 const routes: Array<
-  {
-    paths: string[];
-    handler: DecoHandler;
-    Component?: ComponentType<PageParams>;
-  }
+    {
+        paths: string[];
+        handler: DecoHandler;
+        Component?: ComponentType<PageParams>;
+    }
 > = [
-  {
-    paths: ["/live/_meta", "/deco/meta"],
-    handler: metaHandler,
-  },
-  {
-    paths: ["/live/release", "/.decofile"],
-    handler: releaseHandler,
-  },
-  {
-    paths: ["/live/inspect/:block", "/deco/inspect/:block"],
-    handler: inspectHandler,
-  },
-  {
-    paths: ["/live/invoke", "/deco/invoke"],
-    handler: invokeHandler,
-  },
-  {
-    paths: ["/live/invoke/*", "/deco/invoke/*"],
-    handler: invokeKeyHandler,
-  },
-  {
-    paths: ["/live/previews", "/deco/previews"],
-    handler: previewsHandler,
-    Component: PreviewsPage,
-  },
-  {
-    paths: ["/live/previews/*", "/deco/previews/*"],
-    Component: PreviewPage,
-    handler: previewHandler,
-  },
-  {
-    paths: ["/live/workflows/run", "/deco/workflows/run"],
-    handler: workflowHandler,
-  },
-  {
-    paths: ["/deco/render"],
-    handler: renderHandler,
-    Component: Render,
-  },
-  {
-    paths: ["/", "*"],
-    handler: entrypoint,
-    Component: Render,
-  },
+    {
+        paths: ["/live/_meta", "/deco/meta"],
+        handler: metaHandler,
+    },
+    {
+        paths: ["/live/release", "/.decofile"],
+        handler: releaseHandler,
+    },
+    {
+        paths: ["/live/inspect/:block", "/deco/inspect/:block"],
+        handler: inspectHandler,
+    },
+    {
+        paths: ["/live/invoke", "/deco/invoke"],
+        handler: invokeHandler,
+    },
+    {
+        paths: ["/live/invoke/*", "/deco/invoke/*"],
+        handler: invokeKeyHandler,
+    },
+    {
+        paths: ["/live/previews", "/deco/previews"],
+        handler: previewsHandler,
+        Component: PreviewsPage,
+    },
+    {
+        paths: ["/live/previews/*", "/deco/previews/*"],
+        Component: PreviewPage,
+        handler: previewHandler,
+    },
+    {
+        paths: ["/live/workflows/run", "/deco/workflows/run"],
+        handler: workflowHandler,
+    },
+    {
+        paths: ["/deco/render"],
+        handler: renderHandler,
+        Component: Render,
+    },
+    {
+        paths: ["/", "*"],
+        handler: entrypoint,
+        Component: Render,
+    },
 ];
 
 export type { DecoRouteState };
 export const setup = async <
-  TAppManifest extends AppManifest = AppManifest,
-  THonoState extends DecoRouteState<TAppManifest> = DecoRouteState<
-    TAppManifest
-  >,
+    TAppManifest extends AppManifest = AppManifest,
+    THonoState extends DecoRouteState<TAppManifest> = DecoRouteState<
+        TAppManifest
+    >,
 >(
-  hono: Hono<THonoState>,
+    hono: Hono<THonoState>,
 ) => {
-  const manifest = await import(
-    import.meta.resolve(join(Deno.cwd(), "manifest.gen.ts"))
-  ).then((mod) => mod.default);
-  hono.use(
-    liveness,
-    contextProvider({ manifest }),
-    alienRelease,
-    decod,
-    buildDecoState(),
-    ...main,
-  );
-  hono.get(
-    "/deco/dev",
-    upgradeWebSocket(() => {
-      return {
-        onOpen: (_, ws) => {
-          socket = ws.raw as WebSocket ?? null;
-        },
-        onClose: () => {
-          socket = null;
-        },
-      };
-    }),
-  );
-  for (const { paths, handler, Component } of routes) {
-    for (const path of paths) {
-      hono.all(path, (ctx, next) => {
-        if (Component) {
-          ctx.setRenderer((data) => {
-            return Promise.resolve(
-              new Response(
-                renderToString(
-                  <>
-                    <script
-                      dangerouslySetInnerHTML={{
-                        __html: `
+    const manifest = await import(
+        import.meta.resolve(join(Deno.cwd(), "manifest.gen.ts"))
+    ).then((mod) => mod.default);
+    hono.use(
+        liveness,
+        contextProvider({ manifest }),
+        alienRelease,
+        decod,
+        buildDecoState(),
+        ...main,
+    );
+    hono.get(
+        DEV_SERVER_PATH,
+        upgradeWebSocket(() => {
+            return {
+                onOpen: (_, ws) => {
+                    socket = ws.raw as WebSocket ?? null;
+                },
+                onClose: () => {
+                    socket = null;
+                },
+            };
+        }),
+    );
+    for (const { paths, handler, Component } of routes) {
+        for (const path of paths) {
+            hono.all(path, (ctx, next) => {
+                if (Component) {
+                    ctx.setRenderer((data) => {
+                        return Promise.resolve(
+                            new Response(
+                                renderToString(
+                                    <>
+                                        <script
+                                            dangerouslySetInnerHTML={{
+                                                __html: `
         // Debounce function to limit the rate of page refreshes
         function debounce(func, delay) {
             let timeoutId;
@@ -162,7 +163,7 @@ export const setup = async <
             // Construct WebSocket URL based on current domain and protocol
             const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
             const host = window.location.host;
-            const path = '/deco/dev';
+            const path = '${DEV_SERVER_PATH}';
             const wsUrl = \`\${protocol}//\${host}\${path}\`;
 
             // Create a WebSocket connection
@@ -188,33 +189,33 @@ export const setup = async <
         // Run the setup function when the page loads
         window.onload = setupWebSocket;
                                             `,
-                      }}
-                    >
-                    </script>
-                    <Component
-                      params={ctx.req.param()}
-                      url={new URL(ctx.req.url)}
-                      data={data}
-                    />
-                  </>,
-                ),
-                {
-                  status: 200,
-                  headers: { "Content-Type": "text/html" },
-                },
-              ),
-            );
-          });
+                                            }}
+                                        >
+                                        </script>
+                                        <Component
+                                            params={ctx.req.param()}
+                                            url={new URL(ctx.req.url)}
+                                            data={data}
+                                        />
+                                    </>,
+                                ),
+                                {
+                                    status: 200,
+                                    headers: { "Content-Type": "text/html" },
+                                },
+                            ),
+                        );
+                    });
+                }
+                // deno-lint-ignore no-explicit-any
+                return handler(ctx as any, next);
+            });
         }
-        // deno-lint-ignore no-explicit-any
-        return handler(ctx as any, next);
-      });
     }
-  }
 };
 
 addEventListener("hmr", () => {
-  if (socket) {
-    socket.send("refresh");
-  }
+    if (socket) {
+        socket.send("refresh");
+    }
 });
