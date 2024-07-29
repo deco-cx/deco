3cf2607249b7e08a1374422d99a8d7a3afa1913e
gimenes <tlgimenes@gmail.com>
2024-07-29 17:15:42 -0300

    simple git

--- daemon/deps.ts
+++ daemon/deps.ts
@@ -2,5 +2,4 @@ export { FSError, realtimeFor } from "jsr:@deco/realtime@0.1.6";
 export type { File, RealtimeState } from "jsr:@deco/realtime@0.1.6";
 export * as gitIgnore from "npm:ignore@5.3.1";
 
-export * as http from "npm:isomorphic-git@1.27.1/http/web/index.js";
-export * as git from "npm:isomorphic-git@1.27.1";
+export * as GIT from "npm:simple-git@3.25.0";
--- daemon/git.ts
+++ daemon/git.ts
@@ -1,180 +1,58 @@
-import fs from "node:fs";
-// import { git, http } from "./deps.ts";
+import { GIT } from "./deps.ts";
 
-const dir = Deno.cwd();
+const git = GIT.simpleGit(Deno.cwd());
 
-const decoder = new TextDecoder();
+const getMergeBase = async () => {
+  const { all: [local, upstream] } = await git.branch();
 
-// git fetch -p
-const gitFetchP = () =>
-  git.fetch({
-    fs,
-    http,
-    dir,
-    depth: 5,
-    prune: true,
-    pruneTags: true,
-  });
-
-// git reset .
-const gitResetDot = async () => {
-  const matrix = await git.statusMatrix({ fs, dir });
-
-  // Reset all staged files
-  await Promise.all(
-    matrix
-      .filter(([_filepath, _head, _workdir, stage]) => stage !== 1)
-      .map(([filepath]) => git.resetIndex({ fs, dir, filepath })),
-  );
-};
-
-const resolveRefs = async () => {
-  const branch = await git.currentBranch({ fs, dir });
-
-  if (!branch) {
-    throw new Error("Missing branch");
+  if (!local || !upstream) {
+    throw new Error(`Missing local or upstream branches`);
   }
 
-  const [ours, theirs] = await Promise.all([
-    git.resolveRef({ fs, dir, ref: branch }),
-    git.resolveRef({ fs, dir, ref: `origin/${branch}` }),
-  ]);
+  const base = await git.raw("merge-base", local, upstream);
 
-  const [base] = await git.findMergeBase({
-    fs,
-    dir,
-    oids: [ours, theirs],
-  });
-
-  if (!base) {
-    throw new Error("Missing merge base!");
-  }
-
-  return { ours, theirs, base };
+  return base.trim();
 };
 
-interface GitDiff {
-  /** filename path */
-  path: string;
-  /** git hash */
-  oid: string;
-  /** file content on the upstream branch */
-  from: string | undefined;
-  /** changed file content */
-  to: string | undefined;
-  /** last modified content at (seconds) */
-  mtime: number | null;
+export interface GitDiffAPI {
+  response: GIT.DiffResult;
 }
 
-export interface ChangesetAPI {
-  response: GitDiff[];
-}
+/** Diff between current state and upstream latest common commit */
+const diff = async () => {
+  await git.fetch(["-p"]);
 
-const rebaseAndResetBranches = async () => {
-  // git fetch -p
-  await gitFetchP();
+  const base = await getMergeBase();
+  const { files } = await git.diffSummary([base]);
 
-  // git reset .
-  await gitResetDot();
+  const changeset = await Promise.all(files.map(async (fileDiff) => ({
+    ...fileDiff,
+    ...fileDiff.binary === false && {
+      from: await git.show(`${base}:${fileDiff.file}`),
+      to: fileDiff.binary === false &&
+        await Deno.readTextFile(fileDiff.file).catch(() => null),
+    },
+  })));
 
-  const { ours, theirs, base } = await resolveRefs();
-
-  const matrix = await git.statusMatrix({ fs, dir, ref: base });
-  await Promise.all(
-    matrix
-      .filter(([_filepath, _head, workdir, _stage]) => workdir !== 1)
-      .map(([filepath]) => git.resetIndex({ fs, dir, filepath, ref: base })),
-  );
-
-  // console.log((await git.log({ fs, dir })).map((x) => x.oid));
-
-  // await git.writeRef({ fs, dir, ref: "HEAD", value: base, force: true });
-  // await git.writeRef({ fs, dir, ref: "main", value: base, force: true });
-
-  // console.log(await git.fastForward({ fs, http, dir }));
-  // await gitResetDot()
-  // await git.checkout({ fs, dir, ref: commonRef });
-
-  // await git.updateIndex({
-  //   fs,
-  //   dir,
-  //   filepath: ".",
-  //   oid: commonRef,
-  //   add: true,
-  //   remove: true,
-  // });
-
-  console.log({ ours, theirs, base });
-
-  // await git.writeRef({ fs, dir, ref: ours, value: theirs });
-
-  // await git.add({ fs, dir, filepath: "." });
-  // git reset .
-  // await git.resetIndex({ fs, dir, filepath: "." });
-  // // git commit -m "Rebase"
-  // await git.commit({ fs, dir, message: "Rebase" });
-};
-
-rebaseAndResetBranches().catch(console.error);
-
-const changeset = async () => {
-  // git fetch -p
-  await gitFetchP();
-
-  // git reset .
-  await gitResetDot();
-
-  const { base } = await resolveRefs();
-
-  // Resolve changed filepaths
-  const matrix = await git.statusMatrix({ fs, dir, ref: base });
-  const filepaths = matrix
-    .filter(([_filepath, _head, workdir, _stage]) => workdir !== 1)
-    .map(([filepath]) => filepath);
-
-  const changeSetFiles = new Set(filepaths);
-  const changeSetTree = new Set(
-    filepaths.flatMap((filepath) => {
-      const segments = filepath.split("/");
-      return segments.map((_, index) => segments.slice(0, index + 1).join("/"));
-    }),
-  );
-
-  changeSetTree.add(".");
-
-  // this API requires you to return something to keep visiting a subtree
-  const subtree: GitDiff[] = await git.walk({
-    fs,
-    dir,
-    trees: [git.WORKDIR(), git.TREE({ ref: base })],
-    map: async (
-      path: string,
-      [workdir, tree]: Array<git.WalkerEntry | null>,
-    ) => {
-      if (!changeSetTree.has(path)) {
-        return null;
-      }
-
-      const [workdirBytes, workdirStat, treeBytes] = await Promise.all([
-        workdir?.content(),
-        workdir?.stat(),
-        tree?.content(),
-      ]);
-
-      return {
-        path,
-        oid: await tree?.oid(),
-        from: treeBytes ? decoder.decode(treeBytes) : undefined,
-        to: workdirBytes ? decoder.decode(workdirBytes) : undefined,
-        // last time the file was modified: https://git-scm.com/docs/index-format/2.31.0#:~:text=32%2Dbit%20mtime%20seconds
-        mtime: workdirStat?.mtimeSeconds,
-      };
+  return new Response(JSON.stringify(changeset), {
+    status: 200,
+    headers: {
+      "content-type": "application/json",
     },
   });
+};
 
-  const changeSet = subtree.filter((entry) => changeSetFiles.has(entry.path));
+export interface GitStatusAPI {
+  response: GIT.StatusResult;
+}
 
-  return new Response(JSON.stringify(changeSet), {
+/** Git status */
+const status = async () => {
+  await git.fetch(["-p"]);
+
+  const status = await git.status();
+
+  return new Response(JSON.stringify(status), {
     status: 200,
     headers: { "content-type": "application/json" },
   });
@@ -198,18 +76,20 @@ export interface PublishAPI {
 const publish = async (req: Request) => {
   const { message, author } = await req.json() as PublishAPI["body"];
 
-  const matrix = await git.statusMatrix({ fs, dir });
+  await git.fetch(["-p"]);
 
-  // Add all unstaged files
-  await Promise.all(
-    matrix
-      .filter(([_filepath, _head, workdir, _stage]) => workdir !== 1)
-      .map(([filepath]) => git.add({ fs, dir: dir, filepath })),
-  );
+  const base = await getMergeBase();
 
-  // commit & push
-  const oid = await git.commit({ fs, dir, message, author });
-  await git.push({ fs, http, dir, ref: "HEAD" });
+  const result = await git
+    .reset(["."])
+    .reset([base])
+    .add(["."])
+    .commit(message, {
+      "--author": `${author.name} <${author.email}>`,
+    })
+    .push();
+
+  console.log({ result });
 
   return new Response(JSON.stringify({ oid }), {
     status: 200,
@@ -226,11 +106,13 @@ export interface CheckoutAPI {
 const discard = async (req: Request) => {
   const { filepaths } = await req.json() as CheckoutAPI["body"];
 
-  await Promise.all(
-    filepaths.map((filepath) =>
-      git.resetIndex({ fs, dir, ref: "HEAD", filepath })
-    ),
-  );
+  await git.fetch(["-p"]);
+
+  const base = await getMergeBase();
+
+  git.reset(["."])
+    .reset([base])
+    .checkout(filepaths);
 
   return new Response(null, { status: 204 });
 };
@@ -239,80 +121,32 @@ export interface RebaseAPI {
 }
 
 const rebase = async () => {
-  await gitFetchP();
+  await git
+    .fetch(["-p"])
+    .add(".")
+    .commit("Before rebase")
+    .pull({ "--rebase": null, "--strategy-option": "theirs" });
 
-  const currentBranch = await git.currentBranch({ fs, dir, fullname: false });
-
-  await git.fastForward({
-    fs,
-    dir,
-    http,
-    remoteRef: currentBranch ?? "main",
-  });
-
-  console.log("forwarded e agora?");
+  const base = await getMergeBase();
+  await git.reset([base]);
 
   return new Response(null, { status: 204 });
-};
-
-export interface PublishableAPI {
-  response: {
-    status: "ahead" | "behind" | "diverged" | "up-to-date";
-    localHash: string;
-    remoteHash: string;
-  };
-}
-
-const canPublish = async (_req: Request) => {
-  // Step 1: Fetch the latest changes from the remote
-  await gitFetchP();
-
-  // Step 2: Get the current branch name
-  const currentBranch = await git.currentBranch({ fs, dir, fullname: false });
-  const ref = currentBranch ?? "HEAD";
-
-  // Step 3: Get the commit hashes of the local and remote branches
-  const localHash = await git.resolveRef({ fs, dir, ref });
-  const remoteHash = await git.resolveRef({ fs, dir, ref: `origin/${ref}` });
-
-  // Step 4: Compare the local branch with the remote branch
-  const isAhead = await git.isDescendent({
-    fs,
-    dir,
-    oid: localHash,
-    ancestor: remoteHash,
-  });
-  const isBehind = await git.isDescendent({
-    fs,
-    dir,
-    oid: remoteHash,
-    ancestor: localHash,
-  });
-
-  const status = isAhead && !isBehind
-    ? "ahead"
-    : !isAhead && isBehind
-    ? "behind"
-    : !isAhead && !isBehind && localHash !== remoteHash
-    ? "diverged"
-    : "up-to-datse";
-
-  return new Response(JSON.stringify({ status, localHash, remoteHash }), {
-    status: 200,
-    headers: { "content-type": "application/json" },
-  });
 };
 
 export const handler = async (req: Request) => {
   const url = new URL(req.url);
 
   try {
-    if (url.pathname.startsWith("/git/changeset")) {
-      return await changeset();
+    if (url.pathname.startsWith("/git/status")) {
+      return await status();
+    }
+
+    if (url.pathname.startsWith("/git/diff")) {
+      return await diff();
     }
 
     if (url.pathname.startsWith("/git/publish")) {
-      return req.method === "GET" ? await canPublish(req) : await publish(req);
+      return await publish(req);
     }
 
     if (url.pathname.startsWith("/git/discard")) {
