4858970fe0162b8a825c14e865c295f9d1b51fce
Marcos Candeia <marrcooos@gmail.com>
2024-08-05 21:22:27 -0300

    Incorporate form data changes into invoke
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- runtime/routes/invoke.ts
+++ runtime/routes/invoke.ts
@@ -1,8 +1,78 @@
+import { formDataToProps } from "../../clients/formdata.ts";
 import { bodyFromUrl } from "../../utils/http.ts";
 import { invokeToHttpResponse } from "../../utils/invoke.ts";
 import type { InvokeFunction } from "../../utils/invoke.types.ts";
 import { createHandler } from "../middleware.ts";
+/**
+ * All props parsing strategies supported by the invoke endpoint.
+ * To infer a valid strategy for a request, the `getParsingStrategy` function is used.
+ */
+export const propsParsers = {
+  "json": async (req: Request) => await req.json() as Record<string, unknown>,
+  "try-json": async (req: Request) => {
+    try {
+      return await req.json() as Record<string, unknown>;
+    } catch (err) {
+      console.warn("Error parsing props from request", err);
+      return {};
+    }
+  },
+  "form-data": async (req: Request) => {
+    const formData = await req.formData();
+    return formDataToProps(formData);
+  },
+  "search-params": (req: Request) => {
+    const url = new URL(req.url);
+    return bodyFromUrl("props", url);
+  },
+};
 
+/**
+ * Gets the `propsParsers` strategy from the given request.
+ */
+function getParsingStrategy(req: Request): keyof typeof propsParsers | null {
+  if (req.method !== "POST") {
+    return "search-params";
+  }
+
+  const contentType = req.headers.get("content-type");
+  const contentLength = req.headers.get("content-length");
+
+  if (contentLength === "0" || !contentLength) {
+    return null;
+  }
+
+  if (!contentType) {
+    return "try-json";
+  }
+
+  if (contentType.startsWith("application/json")) {
+    return "json";
+  }
+
+  if (contentType.startsWith("multipart/form-data")) {
+    return "form-data";
+  }
+
+  return null;
+}
+
+/**
+ * Infers a props parsing strategy from the given request
+ * then parses the props from the request.
+ * If no strategy is found, an empty object is returned.
+ */
+async function parsePropsFromRequest(
+  req: Request,
+): Promise<Record<string, unknown>> {
+  const strategy = getParsingStrategy(req);
+
+  if (!strategy) {
+    return {};
+  }
+
+  return await propsParsers[strategy](req);
+}
 export const handler = createHandler(async (
   ctx,
 ): Promise<Response> => {
@@ -11,11 +81,8 @@ export const handler = createHandler(async (
       "/deco/invoke/",
       "",
     );
-  const props = ctx.req.raw.method === "POST"
-    ? ctx.req.raw.headers.get("content-length") === "0"
-      ? {}
-      : await ctx.req.raw.json()
-    : bodyFromUrl("props", ctx.var.url);
+
+  const props = await parsePropsFromRequest(ctx.req.raw);
 
   const select = (ctx.var.url.searchParams.getAll("select") ??
     []) as InvokeFunction[
@@ -24,7 +91,7 @@ export const handler = createHandler(async (
 
   const resp = await ctx.var.deco.invoke(
     key as InvokeFunction["key"],
-    props as InvokeFunction["props"],
+    props as Required<InvokeFunction>["props"],
     select,
     ctx.var,
   );
