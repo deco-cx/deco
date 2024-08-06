5a624aeab4c3098e1f6de11e52ced7dda4b78abb
Viktor Marinho <56888067+viktormarinho@users.noreply.github.com>
2024-08-05 21:14:02 -0300

    first class multipart support (#733)
    * add multipart prop to request init. auto handle props -> formdata & formdata -> props
    
    * refactor + docs
    
    * more doc
    
    * suggestion

--- runtime/fresh/routes/invoke.ts
+++ runtime/fresh/routes/invoke.ts
@@ -1,10 +1,82 @@
 import type { HandlerContext } from "$fresh/server.ts";
+import { formDataToProps } from "deco/clients/formdata.ts";
 import type { DecoSiteState, DecoState } from "../../../types.ts";
 import { bodyFromUrl } from "../../../utils/http.ts";
 import { invokeToHttpResponse } from "../../../utils/invoke.ts";
 import type { InvokeFunction } from "../../../utils/invoke.types.ts";
 
 import { payloadToResolvable, wrapInvokeErr } from "./batchInvoke.ts";
+
+/**
+ * All props parsing strategies supported by the invoke endpoint.
+ * To infer a valid strategy for a request, the `getParsingStrategy` function is used.
+ */
+export const propsParsers = {
+  "json": async (req: Request) => await req.json() as Record<string, any>,
+  "try-json": async (req: Request) => {
+    try {
+      return await req.json() as Record<string, any>;
+    } catch (err) {
+      console.warn("Error parsing props from request", err);
+      return {};
+    }
+  },
+  "form-data": async (req: Request) => {
+    const formData = await req.formData();
+    return formDataToProps(formData);
+  },
+  "search-params": async (req: Request) => {
+    const url = new URL(req.url);
+    return bodyFromUrl("props", url);
+  },
+};
+
+/**
+ * Gets the `propsParsers` strategy from the given request.
+ */
+function getParsingStrategy(req: Request): keyof typeof propsParsers | null {
+  if (req.method !== "POST") {
+    return "search-params";
+  }
+
+  const contentType = req.headers.get("content-type");
+  const contentLength = req.headers.get("content-length");
+
+  if (contentLength === "0" || !contentLength) {
+    return null;
+  }
+
+  if (!contentType) {
+    return "try-json";
+  }
+
+  if (contentType.startsWith("application/json")) {
+    return "json";
+  }
+
+  if (contentType.startsWith("multipart/form-data")) {
+    return "form-data";
+  }
+
+  return null;
+}
+
+/**
+ * Infers a props parsing strategy from the given request
+ * then parses the props from the request.
+ * If no strategy is found, an empty object is returned.
+ */
+async function parsePropsFromRequest(
+  req: Request,
+): Promise<Record<string, any>> {
+  const strategy = getParsingStrategy(req);
+
+  if (!strategy) {
+    return {};
+  }
+
+  return await propsParsers[strategy](req);
+}
 
 export const handler = async (
   req: Request,
@@ -14,9 +86,7 @@ export const handler = async (
   >,
 ): Promise<Response> => {
   const url = new URL(req.url); // TODO(mcandeia) check if ctx.url can be used here
-  const props = req.method === "POST"
-    ? req.headers.get("content-length") === "0" ? {} : await req.json()
-    : bodyFromUrl("props", url);
+  const props = await parsePropsFromRequest(req);
 
   const { state: { resolve } } = ctx;
   const invokeFunc: InvokeFunction = {
