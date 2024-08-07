b0caa2a65724c1ea13f1a45e40d02d16fc8fbd1a
Marcos Candeia <marrcooos@gmail.com>
2024-08-07 12:42:58 -0300

    Less packages, more compatibility
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- mod.ts
+++ mod.ts
@@ -1,5 +1,8 @@
+export * from "./blocks/mod.ts";
 export { forApp, proxy, withManifest } from "./clients/withManifest.ts";
+export * from "./commons/workflows/mod.ts";
 export * as JsonViewer from "./components/JsonViewer.tsx";
+export * from "./deco.ts";
 export type { ValueType, WorkflowGen } from "./deps.ts";
 export type {
   Block,
@@ -9,12 +12,16 @@ export type {
   InstanceOf,
   IntrospectParams,
   ResolvableOf,
-  ResolverLike,
+  ResolverLike
 } from "./engine/block.ts";
 export { asResolved, isDeferred } from "./engine/core/resolver.ts";
 export type { Resolvable, Resolved } from "./engine/core/resolver.ts";
 export { $live, initContext, newContext } from "./engine/manifest/manifest.ts";
 export { Context } from "./live.ts";
+export * from "./runtime/errors.ts";
+export * from "./runtime/mod.ts";
 export { Deco } from "./runtime/mod.ts";
+export * from "./types.ts";
 export { allowCorsFor } from "./utils/http.ts";
 export type { StreamProps } from "./utils/invoke.ts";
+
