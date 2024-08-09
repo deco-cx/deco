4460d277d5c705e20e65ce2dd008d230b643b53f
Marcos Candeia <marrcooos@gmail.com>
2024-08-08 22:01:14 -0300

    Add dev script
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

---
+++ scripts/dev.ts
@@ -0,0 +1,27 @@
+import { join } from "@std/path";
+import denoJSON from "../deno.json" with { type: "json" };
+
+const name = denoJSON.name;
+const exports = denoJSON.exports;
+const resetOrDecoFolder = Deno.args[0];
+
+const denoJSONPath = join(Deno.cwd(), "deno.json");
+const projectDenoJSON: typeof denoJSON = await Deno.readTextFile(
+    denoJSONPath,
+).then((str) => JSON.parse(str));
+const entries: Record<string, string> = projectDenoJSON.imports;
+
+const isReset = resetOrDecoFolder === "$";
+for (const [key, value] of Object.entries(exports)) {
+    const entryKey = key.replace(".", name);
+    if (isReset) {
+        delete entries[entryKey];
+    } else {
+        entries[entryKey] = value.replace(".", resetOrDecoFolder);
+    }
+}
+
+await Deno.writeTextFile(
+    denoJSONPath,
+    JSON.stringify(projectDenoJSON, null, 2),
+);
