dec050caf3a206363b880fb48d651440ea589a24
Marcos Candeia <marrcooos@gmail.com>
2024-08-20 07:34:12 -0300

    Output fetch time
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- scripts/run.ts
+++ scripts/run.ts
@@ -1,8 +1,13 @@
 // TODO: @gimenes use redirect instead of serving this file
 
 const packageName = "@deco/deco";
+const timeout = new AbortController();
+const timeoutId = setTimeout(() => timeout.abort(), 2_000);
+
+const start = performance.now();
 const versions: { latest: string } = await fetch(
     `https://jsr.io/${packageName}/meta.json`,
+    { signal: timeout.signal },
 ).then(
     (resp) => resp.json(),
 ).catch((err) => {
@@ -13,4 +18,12 @@ const versions: { latest: string } = await fetch(
     );
     return { latest: "1" };
 });
+console.log(
+    `%cfetched ${versions.latest} version of ${packageName} in ${
+        (performance.now() - start).toFixed(0)
+    }ms`,
+    "color: gray"
+);
+clearTimeout(timeoutId);
+
 await import(`jsr:${packageName}@${versions.latest}/scripts/run`);
