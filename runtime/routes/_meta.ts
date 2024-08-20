6871a7de2982e3ec6be5d3ab6b4c15c1dd9aa8eb
Marcos Candeia <marrcooos@gmail.com>
2024-08-20 15:43:13 -0300

    Support JSR specifiers on native loader
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- runtime/routes/_meta.ts
+++ runtime/routes/_meta.ts
@@ -5,7 +5,7 @@ export const handler = createHandler(async (ctx) => {
   const ifNoneMatch = ctx.req.raw.headers.get("if-none-match");
 
   const res = await ctx.var.deco.meta(
-    ifNoneMatch
+    ifNoneMatch && ctx.var.url.searchParams.get("waitForChanges") === "true"
       ? {
         ifNoneMatch,
         signal: ctx.req.raw.signal,
