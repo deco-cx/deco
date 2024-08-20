31854454c242d17e62ac838410ec4560c1eaaa14
Marcos Candeia <marrcooos@gmail.com>
2024-08-20 10:17:03 -0300

    Fetch versions from JSR and use latest on run script (#782)
    * Fetch versions from JSR and use latest
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    * Output fetch time
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>
    
    ---------
    
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- scripts/run.ts
+++ scripts/run.ts
@@ -1,2 +1,29 @@
 // TODO: @gimenes use redirect instead of serving this file
-import "jsr:@deco/deco/scripts/run";
+
+const packageName = "@deco/deco";
+const timeout = new AbortController();
+const timeoutId = setTimeout(() => timeout.abort(), 2_000);
+
+const start = performance.now();
+const versions: { latest: string } = await fetch(
+    `https://jsr.io/${packageName}/meta.json`,
+    { signal: timeout.signal },
+).then(
+    (resp) => resp.json(),
+).catch((err) => {
+    console.warn(
+        `could not fetch ${packageName} meta.json`,
+        err,
+        "falling back to major specifier",
+    );
+    return { latest: "1" };
+});
+console.log(
+    `%cfetched ${versions.latest} version of ${packageName} in ${
+        (performance.now() - start).toFixed(0)
+    }ms`,
+    "color: gray"
+);
+clearTimeout(timeoutId);
+
+await import(`jsr:${packageName}@${versions.latest}/scripts/run`);
