94ba8a45661ca6b142b016e0755b9464903dfea6
Igor Brasileiro <brasileiro456@gmail.com>
2024-09-13 13:58:48 -0300

    feat: add pathname and querystrings from href to cacheburst (#838)
    * burst props with href when href is set by user
    
    * Drop adding href to props and add stable href in the cachekey

--- hooks/useSection.ts
+++ hooks/useSection.ts
@@ -7,6 +7,41 @@ import { Murmurhash3 } from "../deps.ts";
 
 const hasher = new Murmurhash3();
 
+// List from cloudflare APO https://developers.cloudflare.com/automatic-platform-optimization/reference/query-parameters
+/** List of querystring that should not vary cache */
+const BLOCKED_QS = new Set<string>([
+  "ref",
+  "fbclid",
+  "fb_action_ids",
+  "fb_action_types",
+  "fb_source",
+  "mc_cid",
+  "mc_eid",
+  "gclid",
+  "dclid",
+  "_ga",
+  "campaignid",
+  "adgroupid",
+  "_ke",
+  "cn-reloaded",
+  "age-verified",
+  "ao_noptimize",
+  "usqp",
+  "mkt_tok",
+  "epik",
+  "ck_subscriber_id",
+]);
+
+/** Returns new props object with prop __cb with `pathname?querystring` from href */
+const createStableHref = (href: string): string => {
+  const hrefUrl = new URL(href!, "http://localhost:8000");
+  hrefUrl.searchParams.forEach((_: string, qsName: string) => {
+    if (BLOCKED_QS.has(qsName)) hrefUrl.searchParams.delete(qsName);
+  });
+  hrefUrl.searchParams.sort();
+  return `${hrefUrl.pathname}?${hrefUrl.search}`;
+};
+
 export type Options<P> = {
   /** Section props partially applied */
   props?: Partial<P extends ComponentType<infer K> ? K : P>;
@@ -19,17 +54,6 @@ export const useSection = <P>(
   { props = {}, href }: Pick<Options<P>, "href" | "props"> = {},
 ): string => {
   const ctx = useContext(SectionContext);
-  const revisionId = ctx?.revision;
-  const vary = ctx?.context.state.vary.build();
-  const cbString = [
-    revisionId,
-    vary,
-    ctx?.deploymentId,
-  ].join("|");
-  hasher.hash(cbString);
-  const cb = hasher.result();
-  hasher.reset();
-
   if (typeof document !== "undefined") {
     throw new Error("Partials cannot be used inside an Island!");
   }
@@ -38,11 +62,24 @@ export const useSection = <P>(
     throw new Error("Missing context in rendering tree");
   }
 
+  const revisionId = ctx?.revision;
+  const vary = ctx?.context.state.vary.build();
   const { request, renderSalt, context: { state: { pathTemplate } } } = ctx;
+
+  const hrefParam = href ?? request.url;
+  const cbString = [
+    revisionId,
+    vary,
+    createStableHref(hrefParam),
+    ctx?.deploymentId,
+  ].join("|");
+  hasher.hash(cbString);
+  const cb = hasher.result();
+  hasher.reset();
 
   const params = new URLSearchParams([
     ["props", JSON.stringify(props)],
-    ["href", href ?? request.url],
+    ["href", hrefParam],
     ["pathTemplate", pathTemplate],
     ["renderSalt", `${renderSalt ?? crypto.randomUUID()}`],
     ["__cb", `${cb}`],
