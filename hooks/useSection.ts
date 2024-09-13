8f3c485691f80d070608ca64b0619362f27bb86b
Igor Brasileiro <brasileiro456@gmail.com>
2024-09-12 22:47:39 -0300

    burst props with href when href is set by user

--- hooks/useSection.ts
+++ hooks/useSection.ts
@@ -6,6 +6,43 @@ import { FieldResolver } from "../engine/core/resolver.ts";
 import { Murmurhash3 } from "../deps.ts";
 
 const hasher = new Murmurhash3();
+/** Cache burst key */
+const CACHE_BURST_KEY: string = "__cb";
+
+// List from cloudflare APO https://developers.cloudflare.com/automatic-platform-optimization/reference/query-parameters
+/** List of querystring that should not vary cache */
+const BLOCKED_QS = new Set<string>([
+  "ref",
+  "fbclid",
+  "fb_action_ids",
+  "fb_action_types",
+  "fb_source",
+  "mc_cid",
+  "mc_eid",
+  "gclid",
+  "dclid",
+  "_ga",
+  "campaignid",
+  "adgroupid",
+  "_ke",
+  "cn-reloaded",
+  "age-verified",
+  "ao_noptimize",
+  "usqp",
+  "mkt_tok",
+  "epik",
+  "ck_subscriber_id"
+]);
+
+/** Returns new props object with prop __cb with `pathname?querystring` from href */
+const createPropsFromHref = ( href: string) => {
+  const hrefUrl = new URL(href!, 'http://localhost:8000');
+  hrefUrl.searchParams.forEach((_: string, qsName: string) => {
+    if (BLOCKED_QS.has(qsName)) hrefUrl.searchParams.delete(qsName)
+  })
+  hrefUrl.searchParams.sort();
+  return { [CACHE_BURST_KEY]: `${hrefUrl.pathname}?${hrefUrl.search}` }
+}
 
 export type Options<P> = {
   /** Section props partially applied */
@@ -40,12 +77,16 @@ export const useSection = <P>(
 
   const { request, renderSalt, context: { state: { pathTemplate } } } = ctx;
 
+  const shouldBurstCache = (href?: string): href is string => href !== undefined && Object.keys(props).length === 0
+
   const params = new URLSearchParams([
-    ["props", JSON.stringify(props)],
+    ["props",
+      // This is an workaround to vary the cache when useSection is used without props
+      JSON.stringify(shouldBurstCache(href) ? createPropsFromHref(href!) : props)],
     ["href", href ?? request.url],
     ["pathTemplate", pathTemplate],
     ["renderSalt", `${renderSalt ?? crypto.randomUUID()}`],
-    ["__cb", `${cb}`],
+    [CACHE_BURST_KEY, `${cb}`],
   ]);
 
   if ((props as { __resolveType?: string })?.__resolveType === undefined) {
