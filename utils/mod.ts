4d052466498fde0345a07a12d035f0cba5f825da
Matheus Gaudencio do Rêgo <matheusgr@users.noreply.github.com>
2024-09-27 09:31:09 -0300

    Avoid mod.ts import on deco/deco/utils (#853)
    * Avoid mod.ts import
    
    * Add readStream to web, start deprecate utils
    
    * Update utils/mod.ts
    
    Co-authored-by: Marcos Candeia <marrcooos@gmail.com>
    
    ---------
    
    Co-authored-by: Marcos Candeia <marrcooos@gmail.com>

--- utils/mod.ts
+++ utils/mod.ts
@@ -7,6 +7,9 @@ export {
 export type { PromiseOrValue } from "../engine/core/utils.ts";
 export { decoManifestBuilder } from "../engine/manifest/manifestGen.ts";
 export { adminUrlFor, isAdmin, resolvable } from "./admin.ts";
+/**
+ * @deprecated since version 1.101.22 import from @deco/deco/web instead
+ */
 export { readFromStream } from "./http.ts";
 export { metabasePreview } from "./metabase.tsx";
 export { tryOrDefault } from "./object.ts";
