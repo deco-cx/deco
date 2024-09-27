3c8c7992b0e24be085216443c6017ba4dc40416b
Matheus Gaudencio do Rêgo <matheusgr@users.noreply.github.com>
2024-09-27 09:23:10 -0300

    Update utils/mod.ts
    Co-authored-by: Marcos Candeia <marrcooos@gmail.com>

--- utils/mod.ts
+++ utils/mod.ts
@@ -8,7 +8,7 @@ export type { PromiseOrValue } from "../engine/core/utils.ts";
 export { decoManifestBuilder } from "../engine/manifest/manifestGen.ts";
 export { adminUrlFor, isAdmin, resolvable } from "./admin.ts";
 /**
- * @deprecated since version 1.101.22
+ * @deprecated since version 1.101.22 import from @deco/deco/web instead
  */
 export { readFromStream } from "./http.ts";
 export { metabasePreview } from "./metabase.tsx";
