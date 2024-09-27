031403f528da2fd82de34ecc860497efc96451c8
Marcos Candeia <marrcooos@gmail.com>
2024-09-27 16:16:30 -0300

    Fix passing manifest on plugins
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- plugins/deco.ts
+++ plugins/deco.ts
@@ -1,12 +1,15 @@
 export * from "../runtime/fresh/plugin.ts";
-import plugin from "../runtime/fresh/plugin.ts";
+import type { AppManifest } from "@deco/deco";
+import { default as plugin, type Options } from "../runtime/fresh/plugin.ts";
 
 /**
  * backwards compatibility with old fresh+deco plugin
  */
-export const plugins = (...opt: Parameters<typeof plugin>) => {
+export const plugins = <TManifest extends AppManifest = AppManifest>(
+  opts: Options<TManifest>,
+) => {
   return [
-    plugin(...opt),
+    plugin(opts),
   ];
 };
 
