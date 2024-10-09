2590f1d38358f7a2be2e1cae8cd5819d807d862d
Matheus Gaudencio do Rêgo <matheusgr@users.noreply.github.com>
2024-10-09 09:36:16 -0300

    Cache api integration (v2) (#856)
    * Add support to deno deploy cache api implementation
    
    Documentation for deno's Edge Cache are available at https://docs.deno.com/deploy/manual/edge-cache/
    
    This commit contains the following alterations:
    * Add CACHE_API as default cache engine, which replaces KV in our DD websites (This does not afect the k8s websites, as they are by default using the FILE_SYSTEM cache).
    * Change LOADER_CACHE_START_THRESHOLD env var to 0
    * Change LOADER_CACHE_SIZE to 1_024_000
    * Add Content-Type field to cached response header
    * Add Content-Length field to cached response header if length is available
    
    * Great cache revamp
    
    * Refact cache
    
    * Enable loader cache by default
    
    * Lint, code readability and better env names
    
    * Fixed env names
    
    * Improve env names and default
    
    * Fix format
    
    ---------
    
    Co-authored-by: Itamar Rocha Filho <itamardprf@gmail.com>

--- runtime/caches/fileSystem.ts
+++ runtime/caches/fileSystem.ts
@@ -1,59 +1,14 @@
 import { existsSync } from "@std/fs";
-import { LRUCache } from "npm:lru-cache@10.2.0";
-import { ValueType } from "../../deps.ts";
-import { logger, tracer } from "../../observability/otel/config.ts";
-import { meter } from "../../observability/otel/metrics.ts";
-import { numToUint8Array, uint8ArrayToNum } from "../utils.ts";
+import { logger } from "../../observability/otel/config.ts";
+
 import {
   assertCanBeCached,
   assertNoOptions,
   withCacheNamespace,
-} from "./common.ts";
+} from "./utils.ts";
 
 const FILE_SYSTEM_CACHE_DIRECTORY =
-  Deno.env.get("FILE_SYSTEM_CACHE_DIRECTORY") ?? undefined;
-
-const MAX_CACHE_SIZE = parseInt(Deno.env.get("MAX_CACHE_SIZE") ?? "1073741824"); // 1 GB max size of cache
-const TTL_AUTOPURGE = Deno.env.get("TTL_AUTOPURGE") !== "false"; // automatically delete expired items
-const TTL_RESOLUTION = parseInt(Deno.env.get("TTL_RESOLUTION") ?? "30000"); // check for expired items every 30 seconds
-
-const downloadDuration = meter.createHistogram(
-  "file_system_cache_download_duration",
-  {
-    description: "file system cache download duration",
-    unit: "ms",
-    valueType: ValueType.DOUBLE,
-  },
-);
-
-const bufferSizeSumObserver = meter.createUpDownCounter("buffer_size_sum", {
-  description: "Sum of buffer sizes",
-  unit: "1",
-  valueType: ValueType.INT,
-});
-
-const failedToDisposeFromCache = meter.createCounter(
-  "failed_to_delete_from_fs_cache",
-  {
-    description: "Counter of failed attempts to delete from file system cache",
-    unit: "1",
-    valueType: ValueType.INT,
-  },
-);
-
-const cacheOptions = {
-  maxSize: MAX_CACHE_SIZE,
-  ttlAutopurge: TTL_AUTOPURGE,
-  ttlResolution: TTL_RESOLUTION,
-  sizeCalculation: (value: Uint8Array) => {
-    return uint8ArrayToNum(value); // return the length of the array
-  },
-  dispose: async (_value: Uint8Array, key: string) => {
-    await Deno.remove(`${FILE_SYSTEM_CACHE_DIRECTORY}/${key}`).catch((err) =>
-      failedToDisposeFromCache.add(1, { err })
-    );
-  },
-};
+  Deno.env.get("FILE_SYSTEM_CACHE_DIRECTORY") ?? "/tmp/deco_cache";
 
 // Function to convert headers object to a Uint8Array
 function headersToUint8Array(headers: [string, string][]) {
@@ -102,8 +57,6 @@ function getIterableHeaders(headers: Uint8Array) {
   return filteredHeaders;
 }
 
-const fileCache = new LRUCache(cacheOptions);
-
 function createFileSystemCache(): CacheStorage {
   let isCacheInitialized = false;
   async function assertCacheDirectory() {
@@ -122,19 +75,12 @@ function createFileSystemCache(): CacheStorage {
   async function putFile(
     key: string,
     responseArray: Uint8Array,
-    expires: string,
   ) {
     if (!isCacheInitialized) {
       await assertCacheDirectory();
     }
     const filePath = `${FILE_SYSTEM_CACHE_DIRECTORY}/${key}`;
 
-    const expirationTimestamp = Date.parse(expires); // Convert expires string to a number representing the expiration timestamp
-    const ttl = expirationTimestamp - Date.now(); // Calculate the time to live (ttl) by subtracting the current timestamp from the expiration timestamp
-
-    fileCache.set(key, numToUint8Array(responseArray.length), {
-      ttl: ttl, // Set the ttl of the file added
-    }); // Add to cache, which may trigger disposal of old item
     await Deno.writeFile(filePath, responseArray);
     return;
   }
@@ -144,10 +90,6 @@ function createFileSystemCache(): CacheStorage {
       await assertCacheDirectory();
     }
     try {
-      if (fileCache.has(key)) {
-        // Update the access time in the cache
-        fileCache.get(key);
-      }
       const filePath = `${FILE_SYSTEM_CACHE_DIRECTORY}/${key}`;
       const fileContent = await Deno.readFile(filePath);
       return fileContent;
@@ -228,38 +170,19 @@ function createFileSystemCache(): CacheStorage {
         ): Promise<Response | undefined> => {
           assertNoOptions(options);
           const cacheKey = await requestURLSHA1(request);
-          const span = tracer.startSpan("file-system-get", {
-            attributes: {
-              cacheKey,
-            },
-          });
-          try {
-            const startTime = performance.now();
-            const data = await getFile(cacheKey);
-            const downloadDurationTime = performance.now() - startTime;
+          const data = await getFile(cacheKey);
 
-            span.addEvent("file-system-get-data");
-
-            if (data === null) {
-              return undefined;
-            }
-
-            downloadDuration.record(downloadDurationTime, {
-              bufferSize: data.length,
-            });
-
-            const { headers, body } = extractCombinedBuffer(data);
-            const iterableHeaders = getIterableHeaders(headers);
-            const responseHeaders = new Headers(iterableHeaders);
-            return new Response(
-              body,
-              { headers: responseHeaders },
-            );
-          } catch (err) {
-            throw err;
-          } finally {
-            span.end();
+          if (data === null) {
+            return undefined;
           }
+
+          const { headers, body } = extractCombinedBuffer(data);
+          const iterableHeaders = getIterableHeaders(headers);
+          const responseHeaders = new Headers(iterableHeaders);
+          return new Response(
+            body,
+            { headers: responseHeaders },
+          );
         },
         /** [MDN Reference](https://developer.mozilla.org/docs/Web/API/Cache/matchAll) */
         matchAll: (
@@ -281,10 +204,10 @@ function createFileSystemCache(): CacheStorage {
           }
 
           const cacheKey = await requestURLSHA1(request);
+
           const bodyBuffer = await response.arrayBuffer()
             .then((buffer) => new Uint8Array(buffer))
             .then((buffer) => {
-              bufferSizeSumObserver.add(buffer.length);
               return buffer;
             });
           const headersBuffer = headersToUint8Array([
@@ -292,38 +215,14 @@ function createFileSystemCache(): CacheStorage {
           ]);
           const buffer = generateCombinedBuffer(bodyBuffer, headersBuffer);
 
-          const span = tracer.startSpan("file-system-put", {
-            attributes: {
-              cacheKey,
+          await putFile(
+            cacheKey,
+            buffer,
+          ).catch(
+            (err) => {
+              console.error("file system error", err);
             },
-          });
-
-          try {
-            try {
-              const setSpan = tracer.startSpan("file-system-set", {
-                attributes: { cacheKey },
-              });
-              await putFile(
-                cacheKey,
-                buffer,
-                response.headers.get("expires") ?? "",
-              ).catch(
-                (err) => {
-                  console.error("file system error", err);
-                  setSpan.recordException(err);
-                },
-              ).finally(() => {
-                setSpan.end();
-              }); // do not await for setting cache
-            } catch (error) {
-              logger.error(`error saving to file system ${error?.message}`);
-            }
-          } catch (err) {
-            span.recordException(err);
-            throw err;
-          } finally {
-            span.end();
-          }
+          );
         },
       });
     },
