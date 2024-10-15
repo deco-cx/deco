c957a1c9a2ac708e5434fb35cadcfb67145b2a40
Marcos Candeia <marrcooos@gmail.com>
2024-10-15 10:34:01 -0300

    Upgrade fresh to 1.7.3
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- scripts/codemod.ts
+++ scripts/codemod.ts
@@ -9,7 +9,8 @@ import { jsrLatest } from "@deco/codemod-toolkit/deno-json";
 import { join } from "@std/path/join";
 
 const DECO_PKGS = /(@deco\/.*)|(deco)|(\$live)/;
-const PKGS_TO_CHECK = /(apps)|(deco-sites\/.*\/$)|(partytown)|(@deco\/warp)/;
+const PKGS_TO_CHECK =
+  /(\$fresh)|(apps)|(deco-sites\/.*\/$)|(partytown)|(@deco\/warp)/;
 
 const upgradeDecoVersion = upgradeDeps(DECO_PKGS, true);
 
