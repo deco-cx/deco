f95633b13990cf8060b6977758e6a994ecce0a7f
Marcos Candeia <marrcooos@gmail.com>
2024-11-25 16:47:31 -0300

    Fix explicit return type
    Signed-off-by: Marcos Candeia <marrcooos@gmail.com>

--- dev/mod.ts
+++ dev/mod.ts
@@ -4,7 +4,7 @@ import { join } from "@std/path";
 import { build as tailwindBuild } from "./tailwind.ts";
 
 const DECO_FOLDER = ".deco";
-export const build = async () => {
+export const build = async (): Promise<void> => {
   await tailwindBuild();
   const decoFolder = join(Deno.cwd(), DECO_FOLDER);
   if (
