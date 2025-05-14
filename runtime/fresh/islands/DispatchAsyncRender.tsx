5ccedb728190eccaf16c66ae4ee5d2853ea10ef3
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2025-05-14 15:29:32 -0400

    refactor: simulate same behavior as htmx

--- runtime/fresh/islands/DispatchAsyncRender.tsx
+++ runtime/fresh/islands/DispatchAsyncRender.tsx
@@ -6,17 +6,38 @@ interface Props {
 
 export default function DispatchAsyncRender({ id }: Props) {
   useEffect(function clickOnAsyncRenderElements() {
-    const elem = document.getElementById(id);
-    const parent = elem?.parentElement;
+    let timeout: number | undefined;
 
-    if (elem == null || parent == null) {
-      console.error(
-        `Missing element of id ${id} or its parent element. Async rendering will NOT work properly`,
-      );
-      return;
+    function dispatch() {
+      timeout = setTimeout(() => {
+        const elem = document.getElementById(id);
+        const parent = elem?.parentElement;
+
+        if (elem == null || parent == null) {
+          console.error(
+            `Missing element of id ${id} or its parent element. Async rendering will NOT work properly`,
+          );
+          return;
+        }
+
+        elem.click();
+      }, 6000);
+
+      return () => clearTimeout(timeout);
     }
 
-    elem.click();
+    if (document.readyState === "complete") {
+      return dispatch();
+    }
+
+    document.addEventListener("DOMContentLoaded", dispatch);
+
+    return () => {
+      if (typeof timeout !== "undefined") {
+        clearTimeout(timeout);
+      }
+      document.removeEventListener("DOMContentLoaded", dispatch);
+    };
   }, []);
 
   return <div data-dispatch-async-render />;
