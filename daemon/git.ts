7d3a62fd064d28ab38ede08ec166bc037c681f97
Igor Brasileiro <brasileiro456@gmail.com>
2025-06-26 11:39:18 -0300

    feat: improve copy files from build_files folder (#927)

--- daemon/git.ts
+++ daemon/git.ts
@@ -18,6 +18,7 @@ const SOURCE_PATH = Deno.env.get("SOURCE_ASSET_PATH");
 const DEFAULT_TRACKING_BRANCH = Deno.env.get("DECO_TRACKING_BRANCH") ?? "main";
 const REPO_URL = Deno.env.get("DECO_REPO_URL");
 const GITHUB_APP_KEY = Deno.env.get("GITHUB_APP_KEY");
+const BUILD_FILES_DIR = Deno.env.get("BUILD_FILES_DIR");
 const ADMIN_DOMAIN = "https://admin.deco.cx";
 
 export const lockerGitAPI = createLocker();
@@ -530,13 +531,16 @@ export const ensureGit = async ({ site }: Pick<Options, "site">) => {
       .submoduleInit()
       .submoduleUpdate(["--depth", "1"]);
 
-    const copyBuildFiles = new Deno.Command("cp", {
-      args: ["-r", Deno.env.get("HOME") + "/build-files/.npmrc", "."],
-    });
+    // Copy build files if BUILD_FILES_DIR is specified
+    if (BUILD_FILES_DIR) {
+      const copyBuildFiles = new Deno.Command("cp", {
+        args: ["-r", BUILD_FILES_DIR + "/.", "."],
+      });
 
-    const copy = await copyBuildFiles.output();
-    console.log("stdout", new TextDecoder().decode(copy.stdout));
-    console.log("stderr", new TextDecoder().decode(copy.stderr));
+      const copy = await copyBuildFiles.output();
+      console.log("stdout", new TextDecoder().decode(copy.stdout));
+      console.log("stderr", new TextDecoder().decode(copy.stderr));
+    }
   };
 
   await assertNoIndexLock();
