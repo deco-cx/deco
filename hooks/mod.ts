033d66dff915233577d026d408530f9ca085d3a2
aline-pereira <aline.g7.aa@gmail.com>
2025-11-12 17:50:10 -0300

    changed allowlist approache

--- hooks/mod.ts
+++ hooks/mod.ts
@@ -7,12 +7,6 @@ export { useScript, useScriptAsDataURI } from "./useScript.ts";
 export {
   addAllowedQS as unstable_allowUseSectionHrefQueryStrings,
   addBlockedQS as unstable_blockUseSectionHrefQueryStrings,
-  addBlockedQS as unstable_blockUseSectionQueryStrings,
-  type FilterMode,
-  getFilterConfig as unstable_getQueryStringConfig,
-  type QueryStringFilterConfig,
-  setCustomConfig as unstable_setQueryStringConfig,
-  setFilterMode as unstable_setQueryStringFilterMode,
   useSection,
 } from "./useSection.ts";
 export { useSetEarlyHints } from "./useSetEarlyHints.ts";
--- hooks/useSection.ts
+++ hooks/useSection.ts
@@ -7,13 +7,6 @@ import { Murmurhash3 } from "../deps.ts";
 
 const hasher = new Murmurhash3();
 
-export type FilterMode = "blocklist" | "allowlist";
-
-export interface QueryStringFilterConfig {
-  mode: FilterMode;
-  list: Set<string>;
-}
-
 // List from cloudflare APO https://developers.cloudflare.com/automatic-platform-optimization/reference/query-parameters
 /** List of querystring that should not vary cache */
 const BLOCKED_QS = new Set<string>([
@@ -39,120 +32,31 @@ const BLOCKED_QS = new Set<string>([
   "ck_subscriber_id",
 ]);
 
-const ALLOWED_QS = new Set<string>([
-  // === Product Identification ===
-  "id",
-  "sku",
-  "product_id",
-  "variant_id",
-  "item_id",
+const ALLOWED_QS = new Set<string>();
 
-  // === Product Attributes ===
-  "color",
-  "size",
-  "style",
-  "material",
-  "variant",
-
-  // === Navigation & Pagination ===
-  "page",
-  "limit",
-  "offset",
-  "per_page",
-  "cursor",
-
-  // === Search & Query ===
-  "search",
-  "q",
-  "query",
-  "keyword",
-  "term",
-
-  // === Sorting & Ordering ===
-  "sort",
-  "order",
-  "orderBy",
-  "sortBy",
-  "direction",
-
-  // === Filtering ===
-  "category",
-  "brand",
-  "collection",
-  "filter",
-  "facet",
-  "tag",
-  "type",
-
-  // === Price Range ===
-  "min_price",
-  "max_price",
-  "price_range",
-
-  // === Availability & Status ===
-  "in_stock",
-  "on_sale",
-  "new_arrival",
-  "available",
-
-  // === UI State ===
-  "tab",
-  "section",
-  "modal",
-  "view",
-  "mode",
-
-  // === Discount & Promotions ===
-  "discount",
-  "coupon",
-  "promo_code",
-]);
-
-let currentConfig: QueryStringFilterConfig = {
-  mode: "blocklist",
-  list: BLOCKED_QS,
-};
-
-export const setFilterMode = (mode: FilterMode): void => {
-  currentConfig.mode = mode;
-  currentConfig.list = mode === "blocklist" ? BLOCKED_QS : ALLOWED_QS;
+export const addBlockedQS = (queryStrings: string[]): void => {
+  queryStrings.forEach((qs) => BLOCKED_QS.add(qs));
 };
 
 export const addAllowedQS = (queryStrings: string[]): void => {
   queryStrings.forEach((qs) => ALLOWED_QS.add(qs));
 };
 
-export const addBlockedQS = (queryStrings: string[]): void => {
-  queryStrings.forEach((qs) => BLOCKED_QS.add(qs));
-};
-
-
-export const setCustomConfig = (
-  config: Partial<QueryStringFilterConfig>,
-): void => {
-  currentConfig = { ...currentConfig, ...config };
-};
-
-export const getFilterConfig = (): QueryStringFilterConfig => {
-  return {
-    mode: currentConfig.mode,
-    list: new Set(currentConfig.list), // Return a copy to prevent external mutation
-  };
-};
-
+/** Returns new props object with prop __cb with `pathname?querystring` from href */
 const createStableHref = (href: string): string => {
   const hrefUrl = new URL(href!, "http://localhost:8000");
   const qsList = [...hrefUrl.searchParams.keys()];
 
-  qsList.forEach((qsName: string) => {
-    const shouldRemove = currentConfig.mode === "blocklist"
-      ? currentConfig.list.has(qsName)
-      : !currentConfig.list.has(qsName);
-
-    if (shouldRemove) {
-      hrefUrl.searchParams.delete(qsName);
-    }
-  });
+  if (ALLOWED_QS.size > 0) {
+    qsList.forEach((qsName: string) => {
+      if (!ALLOWED_QS.has(qsName)) hrefUrl.searchParams.delete(qsName);
+    });
+  } else {
+    
+    qsList.forEach((qsName: string) => {
+      if (BLOCKED_QS.has(qsName)) hrefUrl.searchParams.delete(qsName);
+    });
+  }
 
   hrefUrl.searchParams.sort();
   return hrefUrl.href;
