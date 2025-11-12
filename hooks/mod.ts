63d23fb854cb3515e22045a83632050d7a75514a
aline-pereira <aline.g7.aa@gmail.com>
2025-11-12 13:55:10 -0300

    feat: added allowlist option

--- hooks/mod.ts
+++ hooks/mod.ts
@@ -5,7 +5,14 @@ export {
 } from "./usePartialSection.ts";
 export { useScript, useScriptAsDataURI } from "./useScript.ts";
 export {
+  addAllowedQS as unstable_allowUseSectionHrefQueryStrings,
   addBlockedQS as unstable_blockUseSectionHrefQueryStrings,
+  addBlockedQS as unstable_blockUseSectionQueryStrings,
+  type FilterMode,
+  getFilterConfig as unstable_getQueryStringConfig,
+  type QueryStringFilterConfig,
+  setCustomConfig as unstable_setQueryStringConfig,
+  setFilterMode as unstable_setQueryStringFilterMode,
   useSection,
 } from "./useSection.ts";
 export { useSetEarlyHints } from "./useSetEarlyHints.ts";
