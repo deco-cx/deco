53fbde5e7a4d7226716b7efb6b539b6d9c1bab8f
guitavano <tavano62@gmail.com>
2025-12-11 12:27:10 -0300

    remove cache from fastly (#1016)

--- runtime/fetch/mod.ts
+++ runtime/fetch/mod.ts
@@ -1,5 +1,3 @@
-import { ENABLE_LOADER_CACHE } from "../caches/mod.ts";
-import { createFetch as withCache } from "./fetchCache.ts";
 import { createFetch as withLogs } from "./fetchLog.ts";
 
 interface FechInfo {
@@ -22,7 +20,6 @@ interface FechInfo {
 
 export const fetch: FechInfo = [
   withLogs,
-  ENABLE_LOADER_CACHE ? undefined : withCache,
 ].filter(Boolean).reduceRight((acc, curr) => curr!(acc), globalThis.fetch);
 
 export type { DecoRequestInit as RequestInit } from "./fetchCache.ts";
