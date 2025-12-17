0c53d0bf37de4b38f5691ae14f366eed18a86919
guitavano <tavano62@gmail.com>
2025-12-17 14:48:35 -0300

    allow cors csp

--- runtime/routes/release.ts
+++ runtime/routes/release.ts
@@ -1,14 +1,29 @@
+import { getAllowedAuthorities } from "../../engine/trustedAuthority.ts";
+import { allowCorsForOrigin } from "../../utils/http.ts";
 import { createHandler } from "../middleware.ts";
 
 export const handler = createHandler(async (
-  { var: state },
+  { var: state, req },
 ) => {
+  const origin = req.raw.headers.get("origin");
+  const headers: Record<string, string> = {
+    "Content-Type": "application/json",
+  };
+
+  if (origin) {
+    try {
+      const originUrl = new URL(origin);
+      const allowedAuthorities = getAllowedAuthorities();
+      if (allowedAuthorities.includes(originUrl.hostname)) {
+        Object.assign(headers, allowCorsForOrigin(origin));
+      }
+    } catch {
+      // Invalid origin URL, ignore
+    }
+  }
+
   return new Response(
     JSON.stringify(await state.release.state()),
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    },
+    { headers },
   );
 });
