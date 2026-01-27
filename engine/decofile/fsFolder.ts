cded0f70317105a8033cd49b5708c8c1229462c3
guitavano <tavano62@gmail.com>
2026-01-27 17:21:38 -0300

    Tavano/remove unused tailwind env var (#1038)
    * remove unused tailwind env var
    
    * fmt

--- engine/decofile/fsFolder.ts
+++ engine/decofile/fsFolder.ts
@@ -146,7 +146,7 @@ export const newFsFolderProviderFromPath = (
         );
       }
       await Promise.all(promises);
-      
+
       return {
         state: decofile,
         revision: hashState(decofile),
